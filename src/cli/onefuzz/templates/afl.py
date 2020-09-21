e03084313d82fdcb8100194bad467e7970e8c91b
Brian Caswell <bmc@shmoo.com>
2020-09-21 13:42:12 -0400

    enable adding docs for subcommand arguments in the CLI

--- src/cli/onefuzz/templates/afl.py
+++ src/cli/onefuzz/templates/afl.py
@@ -47,12 +47,16 @@ class AFL(Command):
         tags: Optional[Dict[str, str]] = None,
         wait_for_running: bool = False,
         wait_for_files: Optional[List[ContainerType]] = None,
-        fuzzer_setup_container: Optional[Container] = None,
+        afl_version: Optional[Container] = None,
         existing_inputs: Optional[Container] = None,
         dryrun: bool = False,
         notification_config: Optional[NotificationConfig] = None,
     ) -> None:
-        """ Basic AFL job """
+        """
+        Basic AFL job
+
+        :param Container afl_version: Specify the AFL container to use in the job
+        """
 
         if existing_inputs:
             self.onefuzz.containers.get(existing_inputs)
@@ -106,13 +110,16 @@ class AFL(Command):
 
         target_exe_blob_name = helper.target_exe_blob_name(target_exe, setup_dir)
 
-        if fuzzer_setup_container is None:
-            fuzzer_setup_container = Container(
+        if afl_version is None:
+            afl_version = Container(
                 "afl-linux" if helper.platform == OS.linux else "afl-windows"
             )
 
+        # verify the AFL container exists
+        self.onefuzz.containers.get(afl_version)
+
         containers = [
-            (ContainerType.tools, fuzzer_setup_container),
+            (ContainerType.tools, afl_version),
             (ContainerType.setup, helper.containers[ContainerType.setup]),
             (ContainerType.crashes, helper.containers[ContainerType.crashes]),
             (ContainerType.inputs, helper.containers[ContainerType.inputs]),
