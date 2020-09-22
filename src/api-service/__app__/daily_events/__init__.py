0f78f50c68405f122066ef4908f82f2af3c494b4
Brian Caswell <bmc@shmoo.com>
2020-09-22 11:34:24 -0400

    fix isort

--- src/api-service/__app__/daily_events/__init__.py
+++ src/api-service/__app__/daily_events/__init__.py
@@ -8,8 +8,8 @@ import logging
 import azure.functions as func
 from onefuzztypes.enums import VmState
 
-from ..onefuzzlib.proxy import Proxy
 from ..onefuzzlib.pools import Scaleset
+from ..onefuzzlib.proxy import Proxy
 
 
 def main(mytimer: func.TimerRequest) -> None:  # noqa: F841
