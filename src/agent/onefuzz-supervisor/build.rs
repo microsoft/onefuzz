b4b21c69e8dd34d61b8ec0ceb13321b37c8b7101
Brian Caswell <bmc@shmoo.com>
2020-09-30 17:33:23 -0400

    set more details in builds

--- src/agent/onefuzz-supervisor/build.rs
+++ src/agent/onefuzz-supervisor/build.rs
@@ -2,11 +2,12 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
+use std::env;
 
 fn run_cmd(args: &[&str]) -> Result<String, Box<dyn Error>> {
     let cmd = Command::new(args[0]).args(&args[1..]).output()?;
     if cmd.status.success() {
-        Ok(String::from_utf8_lossy(&cmd.stdout).to_string())
+        Ok(String::from_utf8_lossy(&cmd.stdout).trim().to_string())
     } else {
         Err(From::from("failed"))
     }
@@ -16,21 +17,43 @@ fn read_file(filename: &str) -> Result<String, Box<dyn Error>> {
     let mut file = File::open(filename)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
+    contents = contents.trim().to_string();
 
     Ok(contents)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn print_version(include_sha: bool) -> Result<(), Box<dyn Error>> {
+    let mut version = read_file("../../../CURRENT_VERSION")?;
     let sha = run_cmd(&["git", "rev-parse", "HEAD"])?;
-    let with_changes = if run_cmd(&["git", "diff", "--quiet"]).is_err() {
-        "-local_changes"
-    } else {
-        ""
-    };
-    println!("cargo:rustc-env=GIT_VERSION={}{}", sha, with_changes);
 
-    let version = read_file("../../../CURRENT_VERSION")?;
+    if include_sha {
+        version.push('-');
+        version.push_str(&sha);
+
+        // if we're a non-release buil, check to see if git has
+        // unstaged changes
+        if run_cmd(&["git", "diff", "--quiet"]).is_err() {
+            version.push('.');
+            version.push_str("localchanges");
+        }
+    }
+
+    println!("cargo:rustc-env=GIT_VERSION={}", sha);    
     println!("cargo:rustc-env=ONEFUZZ_VERSION={}", version);
-
+ 
     Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // If we're built off of a tag, we accept CURRENT_VERSION as is.  Otherwise
+    // modify it to indicate local build
+    if let Ok(val) = env::var("GITHUB_REF") {
+        if val.starts_with("refs/tags/") { 
+            print_version(false)
+        } else {
+            print_version(true)
+        }
+    } else {
+        print_version(true)
+    }
 }
---
+++ src/ci/get-version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+set -e
+
+SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+BASE_VERSION=$(cat ${SCRIPT_DIR}/../../CURRENT_VERSION)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+GIT_HASH=$(git rev-parse HEAD)
+
+if [ '${CI}' == 'true' ]; then
+    TAG_VERSION=${GITHUB_REF#refs/tags/}
+    
+    # this isn't a tag
+    if [ ${TAG_VERSION} == ${GITHUB_REF} ]; then
+        echo ${BASE_VERSION}-${GIT_HASH}
+    else
+        echo ${BASE_VERSION}
+    fi    
+else
+    if $(git diff --quiet); then
+        echo ${BASE_VERSION}-${GIT_HASH}
+    else
+        echo ${BASE_VERSION}-${GIT_HASH}.localchanges
+    fi 
+fi
