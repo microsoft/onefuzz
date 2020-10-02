5bce59c791104c8e291e3908598bced861a346c5
Anshuman Goel <ansgoel@microsoft.com>
2020-10-02 10:23:31 -0700

    encoding added

--- src/cli/build_cli.ps1
+++ src/cli/build_cli.ps1
@@ -9,10 +9,9 @@ try {
     python setup.py sdist bdist_wheel 
     Copy-Item dist/*.whl "$app_dir"
     Set-Location $app_dir
-    (Get-Content -path "requirements.txt") -replace 'onefuzztypes==0.0.0', './onefuzztypes-0.0.0-py3-none-any.whl' | Out-File "requirements.txt"
+    (Get-Content -path "requirements.txt") -replace 'onefuzztypes==0.0.0', './onefuzztypes-0.0.0-py3-none-any.whl' | Out-File -FilePath "requirements.txt" -Encoding "utf8"
     pyinstaller $app_dir/onefuzz/__main__.py --onefile --name onefuzz --additional-hooks-dir extra/pyinstaller --hidden-import='pkg_resources.py2_warn' --exclude-module tkinter --exclude-module PySide2 --exclude-module PIL.ImageDraw --exclude-module Pillow --clean
-    (Get-Content -path "requirements.txt") -replace './onefuzztypes-0.0.0-py3-none-any.whl', 'onefuzztypes==0.0.0' | Out-File "requirements.txt"
-    cat "requirements.txt"
+    (Get-Content -path "requirements.txt") -replace './onefuzztypes-0.0.0-py3-none-any.whl', 'onefuzztypes==0.0.0' | Out-File -FilePath "requirements.txt" -Encoding "utf8"
     Remove-Item 'onefuzztypes-0.0.0-py3-none-any.whl'
 } 
 finally {
