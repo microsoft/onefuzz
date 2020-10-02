f26cd9cf5d85dd5575593f12d321787ff5548cfe
Anshuman Goel <ansgoel@microsoft.com>
2020-10-02 10:18:12 -0700

    build cli

---
+++ src/cli/build_cli.ps1
@@ -0,0 +1,20 @@
+param (
+    [Parameter()]
+    [string] $app_dir = "$PSScriptRoot"
+)
+
+try { 
+    Push-Location
+    Set-Location "$app_dir/../pytypes"
+    python setup.py sdist bdist_wheel 
+    Copy-Item dist/*.whl "$app_dir"
+    Set-Location $app_dir
+    (Get-Content -path "requirements.txt") -replace 'onefuzztypes==0.0.0', './onefuzztypes-0.0.0-py3-none-any.whl' | Out-File "requirements.txt"
+    pyinstaller $app_dir/onefuzz/__main__.py --onefile --name onefuzz --additional-hooks-dir extra/pyinstaller --hidden-import='pkg_resources.py2_warn' --exclude-module tkinter --exclude-module PySide2 --exclude-module PIL.ImageDraw --exclude-module Pillow --clean
+    (Get-Content -path "requirements.txt") -replace './onefuzztypes-0.0.0-py3-none-any.whl', 'onefuzztypes==0.0.0' | Out-File "requirements.txt"
+    cat "requirements.txt"
+    Remove-Item 'onefuzztypes-0.0.0-py3-none-any.whl'
+} 
+finally {
+    Pop-Location
+}
