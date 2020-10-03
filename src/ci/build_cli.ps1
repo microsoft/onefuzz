c2bd04aede86c40b8f73b5dc4ca0679a7e00c60c
Anshuman Goel <ansgoel@microsoft.com>
2020-10-03 11:40:17 -0700

    pr comments

--- src/ci/build_cli.ps1
+++ src/ci/build_cli.ps1
@@ -1,21 +1,39 @@
 param (
     [Parameter()]
-    [string] $app_dir = "$PSScriptRoot",
-    [Parameter()]
-    [string] $version = "0.0.0"
+    [string] $app_dir = "$PSScriptRoot"
 )
 
 try { 
     Push-Location
+
+    # Get Version and Replace versions
+    $version = bash .\get-version.sh
+    bash .\set-versions.sh
+
+    # Create wheel for onefuzztypes
     Set-Location "$app_dir/../pytypes"
     python setup.py sdist bdist_wheel 
     Copy-Item dist/*.whl "$app_dir/../cli"
-    Set-Location "$app_dir/../cli"
-    (Get-Content -path "requirements.txt") -replace "onefuzztypes==0.0.0", "./onefuzztypes-0.0.0-py3-none-any.whl" | Out-File -FilePath "requirements.txt" -Encoding "ascii"
-    pyinstaller onefuzz/__main__.py --onefile --name "onefuzz-$version" --additional-hooks-dir extra/pyinstaller --hidden-import='pkg_resources.py2_warn' --exclude-module tkinter --exclude-module PySide2 --exclude-module PIL.ImageDraw --exclude-module Pillow --clean
-    (Get-Content -path "requirements.txt") -replace "./onefuzztypes-0.0.0-py3-none-any.whl", "onefuzztypes==0.0.0" | Out-File -FilePath "requirements.txt" -Encoding "ascii"
-    Remove-Item "onefuzztypes-0.0.0-py3-none-any.whl"
-    Write-Host "OneFuzz exe is available at dist\onefuzz-$version.exe"
+
+    # setup creates whl file which replace - to _
+    Set-Location "$app_dir/../cli"    
+    $_version = $version -replace "-", "_"
+    Write-Host $version
+
+    # Replace onefuzztypes requirement to whl file
+    (Get-Content -path "requirements.txt") -replace "onefuzztypes==$version", "./onefuzztypes-$_version-py3-none-any.whl" | Out-File -FilePath "requirements.txt" -Encoding "ascii"
+    pip install -r .\requirements.txt -r .\requirements-dev.txt
+
+    # Build exe
+    pyinstaller onefuzz/__main__.py --onefile --name "onefuzz" --additional-hooks-dir extra/pyinstaller --hidden-import='pkg_resources.py2_warn' --exclude-module tkinter --exclude-module PySide2 --exclude-module PIL.ImageDraw --exclude-module Pillow --clean
+    
+    # Cleanup
+    (Get-Content -path "requirements.txt") -replace "./onefuzztypes-$_version-py3-none-any.whl", "onefuzztypes==$version" | Out-File -FilePath "requirements.txt" -Encoding "ascii"
+    Remove-Item "*.whl"
+    Set-Location "$app_dir"
+    bash .\unset-versions.sh
+    
+    Write-Host "OneFuzz exe is available at src\cli\dist\onefuzz.exe"
 } 
 finally {
     Pop-Location
