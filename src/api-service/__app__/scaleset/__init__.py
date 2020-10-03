3fc14a1f03028d89d3c6b38de83d70542c724a30
Brian Caswell <bmc@shmoo.com>
2020-10-03 01:08:42 -0400

    don't use new_size

--- src/api-service/__app__/scaleset/__init__.py
+++ src/api-service/__app__/scaleset/__init__.py
@@ -134,7 +134,7 @@ def patch(req: func.HttpRequest) -> func.HttpResponse:
         )
 
     if request.size is not None:
-        scaleset.new_size = request.size
+        scaleset.size = request.size
         scaleset.state = ScalesetState.resize
 
     scaleset.save()
