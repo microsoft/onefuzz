7e71369e79a24bdafc2d85897df890c7de275991
Brian Caswell <bmc@shmoo.com>
2020-10-12 18:55:39 -0400

    for local testing, just build the agent, don't run the full CI script

--- src/docker/linux/build.sh
+++ src/docker/linux/build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash 
 
+BUILD_DIR=$(dirname $(realpath $0))
+cd ${BUILD_DIR}
+
 rm -rf tools
-cp -r ../../runtime-tools tools
+cp -r ../../runtime-tools ${BUILD_DIR}/tools
 
 if [ ! -d ../../../artifacts/azcopy ]; then
     (cd ../../../; ./src/ci/agent.sh)
 fi
-(cd ../../../; cp artifacts/azcopy/azcopy src/docker/linux/tools/linux)
+(cd ../../../; cp artifacts/azcopy/azcopy ${BUILD_DIR}/tools/linux)
 
 if [ -d ../../../artifacts/agent ]; then
-    (cd ../../../; cp artifacts/agent/onefuzz-agent artifacts/agent/onefuzz-supervisor src/docker/linux/tools/linux)
+    (cd ../../../; cp artifacts/agent/onefuzz-agent artifacts/agent/onefuzz-supervisor ${BUILD_DIR}/tools/linux)
 else
-    (cd ../../agent; cargo build --release; cp target/release/onefuzz-{agent,supervisor} ../src/docker/linux/tools/linux)
+    (cd ../../agent; cargo build --release; cp target/release/onefuzz-{agent,supervisor} ${BUILD_DIR}/tools/linux)
 fi
 
 docker build -t onefuzz:latest .
