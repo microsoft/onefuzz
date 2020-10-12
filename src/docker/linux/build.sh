5bc524350266300f34c033e11f28d5e64da70273
Brian Caswell <bmc@shmoo.com>
2020-10-12 18:30:12 -0400

    enable simple builds of the agent/supervisor when built locally

--- src/docker/linux/build.sh
+++ src/docker/linux/build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash 
 
+rm -rf tools
 cp -r ../../runtime-tools tools
-(cd ../../../; ./src/ci/agent.sh; cp artifacts/agent/onefuzz-agent artifacts/agent/onefuzz-supervisor src/docker/linux/tools/linux)
-(cd ../../../; ./src/ci/azcopy.sh; cp artifacts/azcopy/azcopy src/docker/linux/tools/linux)
+
+if [ ! -d ../../../artifacts/azcopy ]; then
+    (cd ../../../; ./src/ci/agent.sh)
+fi
+(cd ../../../; cp artifacts/azcopy/azcopy src/docker/linux/tools/linux)
+
+if [ -d ../../../artifacts/agent ]; then
+    (cd ../../../; cp artifacts/agent/onefuzz-agent artifacts/agent/onefuzz-supervisor src/docker/linux/tools/linux)
+else
+    (cd ../../agent; cargo build --release; cp target/release/onefuzz-{agent,supervisor} ../src/docker/linux/tools/linux)
+fi
+
 docker build -t onefuzz:latest .
