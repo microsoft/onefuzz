7d3e5417ffc5b1f3c6532bbc8f34a9ed44cfdc61
Brian Caswell <bmc@shmoo.com>
2020-10-13 21:55:11 -0400

    more trials

--- src/ci/radamsa-windows.ps1
+++ src/ci/radamsa-windows.ps1
@@ -2,8 +2,11 @@ $exe = "setup.exe"
 $url = "https://cygwin.com/setup-x86_64.exe"
 $mirror = "http://cygwin.mirror.constant.com"
 $dest = "c:\cygwin"
+write-host "downloading setup"
 Invoke-WebRequest $url -OutFile $exe
+write-host "launching installer"
 Start-Process -wait -FilePath $exe -ArgumentList "-q -n -s $mirror -R $dest"
+write-host "launching install packages"
 Start-Process -wait -FilePath $exe -ArgumentList "-q -n -s $mirror -R $dest -P _autorebase,alternatives,base-cygwin,base-files,bash,binutils,bzip2,ca-certificates,coreutils,`
 crypto-policies,cygutils,cygwin,cygwin-devel,dash,diffutils,editrights,file,findutils,gawk,`
 gcc-core,gcc-g++,getent,grep,groff,gzip,hostname,info,ipc-utils,less,libargp,libatomic1,libattr1,`
@@ -14,8 +17,8 @@ libquadmath0,libreadline7,libsigsegv2,libsmartcols1,libssl1.1,libstdc++6,libtasn
 libuuid1,login,make,man-db,mintty,ncurses,openssl,p11-kit,p11-kit-trust,publicsuffix-list-dafsa,`
 rebase,run,sed,tar,terminfo,terminfo-extra,tzcode,tzdata,util-linux,vim-minimal,w32api-headers,`
 w32api-runtime,wget,which,windows-default-manifest,xz,zlib0"
-# git clone https://gitlab.com/akihe/radamsa
-# cd radamsa
-# git checkout 8121b78fb8f87e869cbeca931964df2b32435eb7
-# c:\cygwin\bin\bash.exe -c "make --debug=v"
-# copy $installRoot\bin\cygwin1.dll bin\cygwin1.dll
+git clone https://gitlab.com/akihe/radamsa
+cd radamsa
+git checkout 8121b78fb8f87e869cbeca931964df2b32435eb7
+c:\cygwin\bin\bash.exe -c "make --debug=v"
+copy $installRoot\bin\cygwin1.dll bin\cygwin1.dll
