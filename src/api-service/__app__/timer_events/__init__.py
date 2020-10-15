808c6b8d6d910f52cda048a49aff3a96b53ddf3f
Brian Caswell <bmc@shmoo.com>
2020-10-15 14:48:39 -0400

    remove queue updates that have been split off

--- src/api-service/__app__/timer_events/__init__.py
+++ src/api-service/__app__/timer_events/__init__.py
@@ -6,66 +6,23 @@
 import logging
 
 import azure.functions as func
-from onefuzztypes.enums import JobState, NodeState, PoolState, TaskState, VmState
+from onefuzztypes.enums import NodeState, PoolState
 
 from ..onefuzzlib.dashboard import get_event
-from ..onefuzzlib.jobs import Job
+from ..onefuzzlib.orm import process_update
 from ..onefuzzlib.pools import Node, Pool
-from ..onefuzzlib.proxy import Proxy
-from ..onefuzzlib.repro import Repro
-from ..onefuzzlib.tasks.main import Task
 
 
 def main(mytimer: func.TimerRequest, dashboard: func.Out[str]) -> None:  # noqa: F841
-    proxies = Proxy.search_states(states=VmState.needs_work())
-    for proxy in proxies:
-        logging.info("requeueing update proxy vm: %s", proxy.region)
-        proxy.queue()
-
-    vms = Repro.search_states(states=VmState.needs_work())
-    for vm in vms:
-        logging.info("requeueing update vm: %s", vm.vm_id)
-        vm.queue()
-
-    tasks = Task.search_states(states=TaskState.needs_work())
-    for task in tasks:
-        logging.info("requeueing update task: %s", task.task_id)
-        task.queue()
-
-    jobs = Job.search_states(states=JobState.needs_work())
-    for job in jobs:
-        logging.info("requeueing update job: %s", job.job_id)
-        job.queue()
-
     pools = Pool.search_states(states=PoolState.needs_work())
     for pool in pools:
-        logging.info("queuing update pool: %s (%s)", pool.pool_id, pool.name)
-        pool.queue()
+        logging.info("update pool: %s (%s)", pool.pool_id, pool.name)
+        process_update(pool)
 
     nodes = Node.search_states(states=NodeState.needs_work())
     for node in nodes:
-        logging.info("queuing update node: %s", node.machine_id)
-        node.queue()
-
-    expired_tasks = Task.search_expired()
-    for task in expired_tasks:
-        logging.info("queuing stop for task: %s", task.job_id)
-        task.queue_stop()
-
-    expired_jobs = Job.search_expired()
-    for job in expired_jobs:
-        logging.info("queuing stop for job: %s", job.job_id)
-        job.queue_stop()
-
-    # Reminder, proxies are created on-demand.  If something is "wrong" with
-    # a proxy, the plan is: delete and recreate it.
-    for proxy in Proxy.search():
-        if not proxy.is_alive():
-            logging.error("proxy alive check failed, stopping: %s", proxy.region)
-            proxy.state = VmState.stopping
-            proxy.save()
-        else:
-            proxy.save_proxy_config()
+        logging.info("update node: %s", node.machine_id)
+        process_update(node)
 
     event = get_event()
     if event:
