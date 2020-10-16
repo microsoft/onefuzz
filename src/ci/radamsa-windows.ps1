c6c16cba08ee69f617869a39e0ca270018f869ea
Brian Caswell <bmc@shmoo.com>
2020-10-16 16:43:51 -0400

    continued dev

--- src/ci/radamsa-windows.ps1
+++ src/ci/radamsa-windows.ps1
@@ -5,9 +5,9 @@ $dest = "c:\cygwin"
 write-host "downloading setup"
 Invoke-WebRequest $url -OutFile $exe
 write-host "launching installer"
-Start-Process -wait -FilePath $exe -ArgumentList "-q -n -s $mirror -R $dest"
+Start-Process -wait -FilePath $exe -ArgumentList "-n -L -R $dest"
 write-host "launching install packages"
-Start-Process -wait -FilePath $exe -ArgumentList "-q -n -s $mirror -R $dest -P _autorebase,alternatives,base-cygwin,base-files,bash,binutils,bzip2,ca-certificates,coreutils,`
+Start-Process -wait -FilePath $exe -ArgumentList "-n -L -R $dest -P _autorebase,alternatives,base-cygwin,base-files,bash,binutils,bzip2,ca-certificates,coreutils,`
 crypto-policies,cygutils,cygwin,cygwin-devel,dash,diffutils,editrights,file,findutils,gawk,`
 gcc-core,gcc-g++,getent,grep,groff,gzip,hostname,info,ipc-utils,less,libargp,libatomic1,libattr1,`
 libblkid1,libbz2_1,libcrypt2,libfdisk1,libffi6,libgc1,libgcc1,libgdbm4,libgmp10,libgnutls30,libgomp1,`
@@ -18,8 +18,8 @@ libuuid1,login,make,man-db,mintty,ncurses,openssl,p11-kit,p11-kit-trust,publicsu
 rebase,run,sed,tar,terminfo,terminfo-extra,tzcode,tzdata,util-linux,vim-minimal,w32api-headers,`
 w32api-runtime,wget,which,windows-default-manifest,xz,zlib0,wget"
 write-host "installing wget"
-Start-Process -wait -FilePath $exe -ArgumentList "-n -s $mirror -R $dest -P wget,curl"
-Start-Process -wait -FilePath $exe -ArgumentList "-n -s $mirror -R $dest -P cygwin-devel"
+Start-Process -wait -FilePath $exe -ArgumentList "-n -L -R $dest -P wget,curl"
+Start-Process -wait -FilePath $exe -ArgumentList "-n -L -R $dest -P cygwin-devel"
 # git clone https://gitlab.com/akihe/radamsa
 # cd radamsa
 # git checkout 8121b78fb8f87e869cbeca931964df2b32435eb7
