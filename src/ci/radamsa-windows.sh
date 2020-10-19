ef3482a86a96585ed87e8d6e500f018608d83322
Brian Caswell <bmc@shmoo.com>
2020-10-19 12:03:20 -0400

    continued

--- src/ci/radamsa-windows.sh
+++ src/ci/radamsa-windows.sh
@@ -3,13 +3,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+# NOTE: This assumes building within msys64/bash
+
 set -ex
 
-mkdir -p artifacts/third-party/radamsa-win64
+export PATH=/usr/bin:$PATH
+pacman -S gcc gzip --noconfirm
 
+mkdir -p artifacts/third-party/radamsa-win64
 git clone https://gitlab.com/akihe/radamsa
 cd radamsa
-# latest checked version
 git checkout 8121b78fb8f87e869cbeca931964df2b32435eb7
 make
 cp LICENCE bin/* ../artifacts/third-party/radamsa-win64
