93a9da773445b1e5c5dc9514117a3d3a13488298
Brian Caswell <bmc@shmoo.com>
2020-10-19 12:32:34 -0400

    copy comment re why this version from radamsa-linux.sh

--- src/ci/radamsa-windows.sh
+++ src/ci/radamsa-windows.sh
@@ -13,6 +13,7 @@ pacman -S gcc gzip --noconfirm
 mkdir -p artifacts/third-party/radamsa-win64
 git clone https://gitlab.com/akihe/radamsa
 cd radamsa
+# latest checked version
 git checkout 8121b78fb8f87e869cbeca931964df2b32435eb7
 make
 cp LICENCE bin/* ../artifacts/third-party/radamsa-win64
