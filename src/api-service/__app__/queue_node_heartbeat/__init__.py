96f1ac9195093ff1962e6ca8317c58bf435b5425
Cheick Keita <kcheick@gmail.com>
2020-10-19 13:33:14 -0700

    fix message node heartbeat parsing

--- src/api-service/__app__/queue_node_heartbeat/__init__.py
+++ src/api-service/__app__/queue_node_heartbeat/__init__.py
@@ -4,6 +4,7 @@
 # Licensed under the MIT License.
 
 import datetime
+import json
 import logging
 
 import azure.functions as func
@@ -17,9 +18,9 @@ from ..onefuzzlib.pools import Node
 def main(msg: func.QueueMessage, dashboard: func.Out[str]) -> None:
     body = msg.get_body()
     logging.info("heartbeat: %s", body)
-
+    raw = json.loads(body)
     try:
-        entry = NodeHeartbeatEntry.parse_obj(body)
+        entry = NodeHeartbeatEntry.parse_obj(raw)
         node = Node.get_by_machine_id(entry.node_id)
         if not node:
             logging.error("invalid node id: %s", entry.node_id)
@@ -27,7 +28,7 @@ def main(msg: func.QueueMessage, dashboard: func.Out[str]) -> None:
         node.heartbeat = datetime.datetime.utcnow()
         node.save()
     except ValidationError:
-        logging.error("invalid node heartbeat: %s", body)
+        logging.error("invalid node heartbeat: %s", raw)
 
     event = get_event()
     if event:
