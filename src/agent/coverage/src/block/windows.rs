86d773aac9704ad302dba2e4e505839a3519957d
Joe Ranweiler <joranwei@microsoft.com>
2020-10-20 17:04:31 -0700

    Stop debugging if callback impls err

--- src/agent/coverage/src/block/windows.rs
+++ src/agent/coverage/src/block/windows.rs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 use std::collections::BTreeMap;
-use std::path::Path;
 use std::process::Command;
 use std::time::{Duration, Instant};
 
@@ -44,33 +43,35 @@ impl BlockCoverageHandler {
         Self { bp_to_block, coverage, max_duration, started, timed_out }
     }
 
-    pub fn pc(&self, dbg: &mut Debugger) -> (u64, Option<SymInfo>) {
+    pub fn pc(&self, dbg: &mut Debugger) -> Result<(u64, Option<SymInfo>)> {
         let rip = iced_x86::Register::RIP;
-        let pc = dbg.read_register_u64(rip).unwrap();
+        let pc = dbg.read_register_u64(rip)?;
         let sym = dbg.get_symbol(pc).ok();
 
-        (pc, sym)
+        Ok((pc, sym))
     }
 
-    fn add_module(&mut self, dbg: &mut Debugger, path: &Path) {
-        let bitset = crate::pe::process_image(path, false);
+    fn add_module(&mut self, dbg: &mut Debugger, module: &Module) {
+        let bitset = crate::pe::process_image(module.path(), false);
+
+        // If we can't add the module, continue debugging.
+        // We don't expect to have symbols for every module.
         if let Err(err) = bitset {
             log::warn!(
                 "cannot record coverage for module = {}, err = {}",
-                path.display(),
+                module.path().display(),
                 err,
             );
             return;
         }
+
+        // Won't panic, due to above check.
         let bitset = bitset.unwrap();
+        let module_coverage = ModuleCoverageBlocks::new(module.path(), module.name(), bitset);
 
-        let path = path.to_owned();
-        let name = path.file_stem().unwrap().to_owned();
-        let module = ModuleCoverageBlocks::new(path, name, bitset);
-
-        let m = self.coverage.add_module(module);
-        let module = &self.coverage.modules()[m];
-        for (b, block) in module.blocks().iter().enumerate() {
+        let m = self.coverage.add_module(module_coverage);
+        let module_coverage = &self.coverage.modules()[m];
+        for (b, block) in module_coverage.blocks().iter().enumerate() {
             let bp = dbg.register_breakpoint(
                 module.name(),
                 block.rva() as u64,
@@ -81,14 +82,16 @@ impl BlockCoverageHandler {
 
         log::debug!(
             "inserted {} breakpoints for module {}",
-            module.blocks().len(),
+            module_coverage.blocks().len(),
             module.path().display(),
         );
     }
-}
 
-impl DebugEventHandler for BlockCoverageHandler {
-    fn on_create_process(&mut self, dbg: &mut Debugger, module: &Module) {
+    fn stop(&self, dbg: &mut Debugger) {
+        dbg.quit_debugging();
+    }
+
+    fn try_on_create_process(&mut self, dbg: &mut Debugger, module: &Module) -> Result<()> {
         dbg.target().sym_initialize().unwrap();
 
         log::info!("exe loaded: {}, {} bytes",
@@ -96,20 +99,24 @@ impl DebugEventHandler for BlockCoverageHandler {
                  module.image_size(),
         );
 
-        self.add_module(dbg, module.path());
+        self.add_module(dbg, module);
+
+        Ok(())
     }
 
-    fn on_load_dll(&mut self, dbg: &mut Debugger, module: &Module) {
+    fn try_on_load_dll(&mut self, dbg: &mut Debugger, module: &Module) -> Result<()> {
         log::info!("dll loaded: {}, {} bytes",
                  module.path().display(),
                  module.image_size(),
         );
 
-        self.add_module(dbg, module.path());
+        self.add_module(dbg, module);
+
+        Ok(())
     }
 
-    fn on_breakpoint(&mut self, dbg: &mut Debugger, bp: BreakpointId) {
-        let (pc, _sym) = self.pc(dbg);
+    fn try_on_breakpoint(&mut self, dbg: &mut Debugger, bp: BreakpointId) -> Result<()> {
+        let (pc, _sym) = self.pc(dbg)?;
 
         if let Some(&(m, b)) = self.bp_to_block.get(&bp) {
             if log::max_level() == log::Level::Trace {
@@ -123,12 +130,42 @@ impl DebugEventHandler for BlockCoverageHandler {
         } else {
             log::error!("no block for breakpoint");
         }
+
+        Ok(())
     }
 
-    fn on_poll(&mut self, dbg: &mut Debugger) {
+    fn try_on_poll(&mut self, dbg: &mut Debugger) -> Result<()> {
         if !self.timed_out && self.started.elapsed() > self.max_duration {
             self.timed_out = true;
             dbg.quit_debugging();
+        }
+
+        Ok(())
+    }
+}
+
+impl DebugEventHandler for BlockCoverageHandler {
+    fn on_create_process(&mut self, dbg: &mut Debugger, module: &Module) {
+        if self.try_on_create_process(dbg, module).is_err() {
+            self.stop(dbg);
+        }
+    }
+
+    fn on_load_dll(&mut self, dbg: &mut Debugger, module: &Module) {
+        if self.try_on_load_dll(dbg, module).is_err() {
+            self.stop(dbg);
+        }
+    }
+
+    fn on_breakpoint(&mut self, dbg: &mut Debugger, bp: BreakpointId) {
+        if self.try_on_breakpoint(dbg, bp).is_err() {
+            self.stop(dbg);
+        }
+    }
+
+    fn on_poll(&mut self, dbg: &mut Debugger) {
+        if self.try_on_poll(dbg).is_err() {
+            self.stop(dbg);
         }
     }
 }
