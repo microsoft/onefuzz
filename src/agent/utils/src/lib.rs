6af9fef231709fa9cde16f82e21df451b23d62fa
Cheick Keita <kcheick@gmail.com>
2020-10-22 19:04:35 -0700

    Added copyright statement

--- src/agent/utils/src/lib.rs
+++ src/agent/utils/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 use anyhow::Result;
 use async_trait::async_trait;
 use backoff::{self, future::FutureOperation, ExponentialBackoff};
