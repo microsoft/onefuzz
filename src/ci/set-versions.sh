4f53d4b049bf6d53ac1c0e24014dc3d2b56af924
Anshuman Goel <ansgoel@microsoft.com>
2020-10-23 09:48:05 -0700

    Adding version as param in script (#177)

--- src/ci/set-versions.sh
+++ src/ci/set-versions.sh
@@ -7,7 +7,7 @@ set -ex
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 GET_VERSION=${SCRIPT_DIR}/get-version.sh
-VERSION=$(${GET_VERSION})
+VERSION=${1:-$(${GET_VERSION})}
 cd ${SCRIPT_DIR}/../../
 
 SET_VERSIONS="src/pytypes/onefuzztypes/__version__.py src/api-service/__app__/onefuzzlib/__version__.py src/cli/onefuzz/__version__.py"
