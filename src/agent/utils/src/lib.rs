305a98187b5b9e4a76fbd9f9aac45ae480c573fb
Cheick Keita <kcheick@gmail.com>
2020-10-22 17:35:17 -0700

    Fixing backoff to retry on http error insead od request send error loggin transient errors

--- src/agent/utils/src/lib.rs
+++ src/agent/utils/src/lib.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
 use async_trait::async_trait;
-use backoff::{self, future::FutureOperation as _, Error, ExponentialBackoff};
+use backoff::{self, future::FutureOperation, ExponentialBackoff};
 use reqwest::Response;
-use std::{io, time::Duration};
+use std::time::Duration;
+use log;
 
 const DEFAULT_RETRY_PERIOD: Duration = Duration::from_millis(500);
 const MAX_ELAPSED_TIME: Duration = Duration::from_secs(60 * 5);
@@ -30,16 +31,22 @@ impl SendRetry for reqwest::RequestBuilder {
         max_elapsed_time: Duration,
     ) -> Result<Response> {
         let op = || async {
-            match self.try_clone() {
-                Some(cloned) => {
-                    let result = cloned.send().await;
-                    Ok(result)
-                }
-                None => Err(Error::Permanent(io::Error::new(
-                    io::ErrorKind::Other,
-                    "This request cannot be retried. Make sure the body is not a stream.",
-                ))),
-            }
+            let cloned = self
+                .try_clone()
+                .ok_or(backoff::Error::Permanent(anyhow::Error::msg(
+                    "this request cannot be cloned",
+                )))?;
+
+            let response = cloned
+                .send()
+                .await
+                .map_err(|err| backoff::Error::Permanent(anyhow::Error::from(err)))?
+                .error_for_status()
+                .map_err(|err| {
+                    log::warn!("Transient error {}", err);
+                    backoff::Error::Transient(anyhow::Error::from(err))
+                })?;
+            Result::<Response, backoff::Error<anyhow::Error>>::Ok(response)
         };
 
         let result = op
@@ -49,56 +56,8 @@ impl SendRetry for reqwest::RequestBuilder {
                 max_elapsed_time: Some(max_elapsed_time),
                 ..ExponentialBackoff::default()
             })
-            .await??;
+            .await?;
 
         Ok(result)
     }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::net::TcpListener;
-
-    // fun server() {
-    //     // todo: randomize the port number
-    //     // note: might need to restric to linux since windows could require address registration at the os level
-
-    //     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    //     for stream in listener.incoming() {
-    //         let stream = stream.unwrap();
-
-    //         let mut buffer = [0; 1024];
-    //         stream.read(&mut buffer).unwrap();
-
-    //         let get = b"GET / HTTP/1.1\r\n";
-
-    //         if buffer.starts_with(get) {
-    //             let contents = fs::read_to_string("hello.html").unwrap();
-
-    //             let response = format!(
-    //                 "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-    //                 contents.len(),
-    //                 contents
-    //             );
-
-    //             stream.write(response.as_bytes()).unwrap();
-    //             stream.flush().unwrap();
-    //         } else {
-    //             // some other request
-    //         }
-    //     }
-
-    // }
-
-    // fn handle_connection(mut stream: TcpStream) {
-
-    // }
-
-    // #[test]
-    // #[tokio::test]
-    // async fn empty_stack() {
-
-    // }
 }
