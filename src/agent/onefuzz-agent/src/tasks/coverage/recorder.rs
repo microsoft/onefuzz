623b76842a76ee1af6cdd06f012bbf91337c4fff
Brian Caswell <bmc@shmoo.com>
2020-10-28 09:37:40 -0400

    use setup/symbols

--- src/agent/onefuzz-agent/src/tasks/coverage/recorder.rs
+++ src/agent/onefuzz-agent/src/tasks/coverage/recorder.rs
@@ -145,6 +145,7 @@ impl CoverageRecorder {
                 .target_exe
                 .parent()
                 .ok_or_else(|| format_err!("invalid path parent"))?
+                .join("symbols")
                 .display()
         );
         if let Some(v) = target_env.get_mut("_NT_SYMBOL_PATH") {
