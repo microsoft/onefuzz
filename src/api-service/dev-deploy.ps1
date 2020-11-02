287b85449ba35db45e29aef5edf8c3ad78e1e1fc
Anshuman Goel <ansgoel@microsoft.com>
2020-11-02 08:25:28 -0800

    pr comment

--- src/api-service/dev-deploy.ps1
+++ src/api-service/dev-deploy.ps1
@@ -7,10 +7,10 @@ param (
     # Specifying version for function
     [Parameter(Mandatory = $false)]
     [string]
-    $version
+    $version = "0.0.0"
 )
 
-try { 
+try {
     Push-Location
     Set-Location "$app_dir/../pytypes"
     python setup.py sdist bdist_wheel 
@@ -19,13 +19,9 @@ try {
     Set-Location __app__
     (New-Guid).Guid | Out-File onefuzzlib/build.id -Encoding ascii
     (Get-Content -path "requirements.txt") -replace 'onefuzztypes==0.0.0', './onefuzztypes-0.0.0-py3-none-any.whl' | Out-File "requirements.txt"
-    if ($version -ne "$null") {
-        (Get-Content -path "onefuzzlib/__version__.py") -replace '__version__ = "0.0.0"', "__version__ = ""$version""" | Out-File "onefuzzlib/__version__.py" -Encoding utf8
-    }
+    (Get-Content -path "onefuzzlib/__version__.py") -replace '__version__ = "0.0.0"', "__version__ = ""$version""" | Out-File "onefuzzlib/__version__.py" -Encoding utf8
     func azure functionapp publish $target --python
-    if ($version -ne "$null") {
-        (Get-Content -path "onefuzzlib/__version__.py") -replace "__version__ = ""$version""", '__version__ = "0.0.0"' | Out-File "onefuzzlib/__version__.py" -Encoding utf8
-    }
+    (Get-Content -path "onefuzzlib/__version__.py") -replace "__version__ = ""$version""", '__version__ = "0.0.0"' | Out-File "onefuzzlib/__version__.py" -Encoding utf8
     (Get-Content -path "requirements.txt") -replace './onefuzztypes-0.0.0-py3-none-any.whl', 'onefuzztypes==0.0.0' | Out-File "requirements.txt"
     Remove-Item 'onefuzztypes-*-py3-none-any.whl'
     Get-Content 'onefuzzlib/build.id'
