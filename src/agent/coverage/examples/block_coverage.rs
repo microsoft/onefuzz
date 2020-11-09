fe639b2656091ed0dd1619adba6de24a16bd6258
Joe Ranweiler <joranwei@microsoft.com>
2020-11-09 22:31:02 +0000

    Add Linux block coverage example

--- src/agent/coverage/examples/block_coverage.rs
+++ src/agent/coverage/examples/block_coverage.rs
@@ -1,24 +1,69 @@
+use std::env;
+
+use anyhow::Result;
+
 #[cfg(target_os = "windows")]
-fn main() {
-    use std::env;
+fn main() -> Result<()> {
     use std::process::Command;
 
     env_logger::init();
 
     let mut args = env::args().skip(1);
-    let exe = args.next().unwrap();
+    let exe = args.next()?;
     let args: Vec<_> = args.collect();
 
     let mut cmd = Command::new(exe);
     cmd.args(&args);
 
-    let coverage = coverage::block::windows::record(cmd).unwrap();
+    let coverage = coverage::block::windows::record(cmd)?;
     let hit = coverage.count_blocks_hit();
     let found = coverage.count_blocks();
     let percent = 100.0 * (hit as f64) / (found as f64);
 
-    println!("block coverage = {}/{} ({:.2}%)", hit, found, percent);
+    log::info!("block coverage = {}/{} ({:.2}%)", hit, found, percent);
+
+    Ok(())
 }
 
 #[cfg(target_os = "linux")]
-fn main() {}
+fn main() -> Result<()> {
+    use coverage::block::linux::record;
+    use pete::Command;
+
+    env_logger::init();
+
+    let argv = env::args().skip(1).collect();
+    let cmd = Command::new(argv)?;
+
+    let coverage = record(cmd)?;
+
+    for m in coverage.modules.values() {
+        let mut hit = 0;
+        let mut found = 0;
+
+        let name = m.module.file_name().unwrap().to_string_lossy();
+
+        log::info!("{}", m.module.display());
+
+        for b in m.blocks.values() {
+            found += 1;
+
+            if b.count > 0 {
+                hit += 1;
+            };
+
+            let marker = if b.count == 0 {
+                " "
+            } else {
+                "x"
+            };
+
+            log::debug!("  [{}] {}+{:x} ({} bytes)", marker, name, b.offset, b.size);
+        }
+
+        let percent = 100.0 * (hit as f64) / (found as f64);
+        log::info!("block coverage = {}/{} ({:.2}%)", hit, found, percent);
+    }
+
+    Ok(())
+}
