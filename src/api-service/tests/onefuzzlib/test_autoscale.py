556b0df135f6a54b273bd2d15426f36786e42f63
Anshuman Goel <ansgoel@microsoft.com>
2020-11-09 15:07:34 -0800

    mypy fix

--- src/api-service/tests/onefuzzlib/test_autoscale.py
+++ src/api-service/tests/onefuzzlib/test_autoscale.py
@@ -4,16 +4,17 @@
 # Licensed under the MIT License.
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+from uuid import UUID
 
-from onefuzztypes.enums import OS, Architecture, TaskType
+from onefuzztypes.enums import OS, Architecture, ContainerType, TaskType
 from onefuzztypes.models import (
-    ContainerType,
     TaskConfig,
     TaskContainers,
     TaskDetails,
     TaskPool,
 )
+from onefuzztypes.primitives import Container, PoolName
 
 from __app__.onefuzzlib.autoscale import autoscale_pool, get_vm_count
 from __app__.onefuzzlib.pools import Pool
@@ -21,24 +22,28 @@ from __app__.onefuzzlib.tasks.main import Task
 
 
 class TestAutoscale(unittest.TestCase):
-    def test_autoscale_pool(self):
+    @patch("__app__.onefuzzlib.tasks.main.Task.get_tasks_by_pool_name")
+    def test_autoscale_pool(self, mock_get_tasks_by_pool_name: MagicMock) -> None:
         pool = Pool(
-            name="test-pool",
-            pool_id="6b049d51-23e9-4f5c-a5af-ff1f73d0d9e9",
+            name=PoolName("test-pool"),
+            pool_id=UUID("6b049d51-23e9-4f5c-a5af-ff1f73d0d9e9"),
             os=OS.linux,
             managed=False,
             arch=Architecture.x86_64,
         )
-        self.assertIsNone(autoscale_pool(pool=pool))
+        autoscale_pool(pool=pool)
+        mock_get_tasks_by_pool_name.assert_not_called()
 
     @patch("__app__.onefuzzlib.tasks.main.Task.get_pool")
-    def test_get_vm_count(self, mock_get_pool):
+    def test_get_vm_count(self, mock_get_pool: MagicMock) -> None:
         self.assertEqual(get_vm_count([]), 0)
 
         task_config = TaskConfig(
-            job_id="6b049d51-23e9-4f5c-a5af-ff1f73d0d9e9",
+            job_id=UUID("6b049d51-23e9-4f5c-a5af-ff1f73d0d9e9"),
             containers=[
-                TaskContainers(type=ContainerType.inputs, name="test-container")
+                TaskContainers(
+                    type=ContainerType.inputs, name=Container("test-container")
+                )
             ],
             tags={},
             task=TaskDetails(
@@ -48,16 +53,16 @@ class TestAutoscale(unittest.TestCase):
                 target_env={},
                 target_options=[],
             ),
-            pool=TaskPool(count=2, pool_name="test-pool"),
+            pool=TaskPool(count=2, pool_name=PoolName("test-pool")),
         )
         task = Task(
-            job_id="6b049d51-23e9-4f5c-a5af-ff1f73d0d9e9",
+            job_id=UUID("6b049d51-23e9-4f5c-a5af-ff1f73d0d9e9"),
             os=OS.linux,
             config=task_config,
         )
         mock_get_pool.return_value = Pool(
-            name="test-pool",
-            pool_id="6b049d51-23e9-4f5c-a5af-ff1f73d0d9e9",
+            name=PoolName("test-pool"),
+            pool_id=UUID("6b049d51-23e9-4f5c-a5af-ff1f73d0d9e9"),
             os=OS.linux,
             managed=False,
             arch=Architecture.x86_64,
