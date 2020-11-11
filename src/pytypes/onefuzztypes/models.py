ca209eb543e10b9d785b6f8be2b6fda9b438abf0
bmc-msft <41130664+bmc-msft@users.noreply.github.com>
2020-11-11 18:28:16 -0500

    refactor agent_events handler (#261)

--- src/pytypes/onefuzztypes/models.py
+++ src/pytypes/onefuzztypes/models.py
@@ -4,7 +4,7 @@
 # Licensed under the MIT License.
 
 from datetime import datetime
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, TypeVar, Union
 from uuid import UUID, uuid4
 
 from pydantic import BaseModel, Field, root_validator, validator
@@ -56,6 +56,10 @@ class EnumModel(BaseModel):
 class Error(BaseModel):
     code: ErrorCode
     errors: List[str]
+
+
+OkType = TypeVar("OkType")
+Result = Union[OkType, Error]
 
 
 class FileEntry(BaseModel):
