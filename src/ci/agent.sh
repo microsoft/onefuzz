693c98885433f4e6841e41b1c36536978a79f520
bmc-msft <41130664+bmc-msft@users.noreply.github.com>
2020-11-12 15:46:32 -0500

    add symbols for windows agents (#306)

--- src/ci/agent.sh
+++ src/ci/agent.sh
@@ -5,6 +5,10 @@
 
 set -ex
 
+exists() {
+    [ -e "$1" ]
+}
+
 export RUSTC_WRAPPER=$(which sccache)
 
 mkdir -p artifacts/agent
@@ -26,3 +30,9 @@ cargo test --release --manifest-path ./onefuzz/Cargo.toml
 
 cp target/release/onefuzz-agent* ../../artifacts/agent
 cp target/release/onefuzz-supervisor* ../../artifacts/agent
+
+if exists target/release/*.pdb; then
+    for file in target/release/*.pdb; do
+        cp ${file} ../../artifacts/agent
+    done
+fi
