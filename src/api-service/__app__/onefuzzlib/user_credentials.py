2d23b9ee4a127e6ab68f7269af67e5d38f8fac39
Brian Caswell <bmc@shmoo.com>
2020-11-12 16:21:39 -0500

    simplify auth header check

--- src/api-service/__app__/onefuzzlib/user_credentials.py
+++ src/api-service/__app__/onefuzzlib/user_credentials.py
@@ -19,21 +19,18 @@ def parse_jwt_token(request: func.HttpRequest) -> Result[UserInfo]:
         return Error(
             code=ErrorCode.INVALID_REQUEST, errors=["Authorization header is expected"]
         )
+
     parts = auth.split()
+
+    if len(parts) != 2:
+        return Error(
+            code=ErrorCode.INVALID_REQUEST, errors=["Invalid authorization header"]
+        )
 
     if parts[0].lower() != "bearer":
         return Error(
             code=ErrorCode.INVALID_REQUEST,
             errors=["Authorization header must start with Bearer"],
-        )
-
-    elif len(parts) == 1:
-        return Error(code=ErrorCode.INVALID_REQUEST, errors=["Token not found"])
-
-    elif len(parts) > 2:
-        return Error(
-            code=ErrorCode.INVALID_REQUEST,
-            errors=["Authorization header must be Bearer token"],
         )
 
     # This token has already been verified by the azure authentication layer
