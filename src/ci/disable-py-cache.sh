595d888cd2a2aa45ebd60f1b91fd851ac9044d3e
Brian Caswell <bmc@shmoo.com>
2020-11-17 17:58:37 -0500

    Address typing issues hidden by memoization.caching

---
+++ src/ci/disable-py-cache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+set -ex
+
+SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+
+sed -i "s/^from memoization import cached/##### from memoization import cached/" $(find . -name '*.py' -not -path .python_packages)
+sed -i "s/^@cached/##### @cached/" $(find . -name '*.py' -not -path .python_packages)
