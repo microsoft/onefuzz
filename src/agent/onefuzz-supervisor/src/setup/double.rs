d47124fe8c6d6c5fc798296e7e37322bedd04dcf
Cheick Keita <kcheick@gmail.com>
2020-11-24 04:43:51 -0800

    Fix state management in the scheduler (#337)

--- src/agent/onefuzz-supervisor/src/setup/double.rs
+++ src/agent/onefuzz-supervisor/src/setup/double.rs
@@ -7,12 +7,16 @@ use super::*;
 pub struct SetupRunnerDouble {
     pub ran: Vec<WorkSet>,
     pub script: SetupOutput,
+    pub error_message: Option<String>,
 }
 
 #[async_trait]
 impl ISetupRunner for SetupRunnerDouble {
     async fn run(&mut self, work_set: &WorkSet) -> Result<SetupOutput> {
         self.ran.push(work_set.clone());
+        if let Some(error) = self.error_message.clone() {
+            anyhow::bail!(error);
+        }
         Ok(self.script.clone())
     }
 }
