fc97c06030a533b40d5e5648d2a17e241076e2c8
Brian Caswell <bmc@shmoo.com>
2020-12-14 14:41:50 -0500

    only download as needed

--- src/ci/azcopy.sh
+++ src/ci/azcopy.sh
@@ -5,13 +5,15 @@
 
 set -ex
 
-if [ ! -f artifacts/azcopy/azcopy ]; then
-    mkdir -p artifacts/azcopy
-    
+mkdir -p artifacts/azcopy
+
+if [ ! -f artifacts/azcopy/azcopy.exe ]; then
     wget -O azcopy.zip https://aka.ms/downloadazcopy-v10-windows
     unzip azcopy.zip
     mv azcopy_windows*/* artifacts/azcopy/
-    
+fi
+
+if [ ! -f artifacts/azcopy/azcopy ]; then
     wget -O azcopy.tgz https://aka.ms/downloadazcopy-v10-linux
     tar zxvf azcopy.tgz
     mv azcopy_linux_amd64*/* artifacts/azcopy/
