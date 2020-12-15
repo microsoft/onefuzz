1402d4f47f93b250dd20b1d6d1fb21fa6dc0a569
Brian Caswell <bmc@shmoo.com>
2020-12-15 10:15:23 -0500

    fix path to aflpp_driver

--- src/ci/aflpp.sh
+++ src/ci/aflpp.sh
@@ -13,6 +13,6 @@ cd AFLplusplus
 git checkout 8e712d1a740b30f9e2d5655d97d4cac6e8aed543
 make
 (cd utils/libdislocator && make)
-(cd examples/aflpp_driver && make); cp examples/aflpp_driver/*.so .
+(cd utils/aflpp_driver && make); cp examples/aflpp_driver/*.so .
 
 cp -rf afl-* *.so *.a *.o dictionaries LICENSE ../artifacts/third-party/aflpp-linux
