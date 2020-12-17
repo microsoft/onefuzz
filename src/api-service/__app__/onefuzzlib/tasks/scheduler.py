bf89457e0ee838058c6a8bec1b018faa5ac695a7
Brian Caswell <bmc@shmoo.com>
2020-12-17 17:04:47 -0500

    remove target_exe again

--- src/api-service/__app__/onefuzzlib/tasks/scheduler.py
+++ src/api-service/__app__/onefuzzlib/tasks/scheduler.py
@@ -56,7 +56,7 @@ class TaskBucketKey(BaseModel):
 def bucket_tasks(tasks: List[Task]) -> Dict[Tuple, List[Task]]:
     # buckets are hashed by:
     # OS, JOB ID, vm sku & image (if available), pool name (if available),
-    # target_exe, if the setup script requires rebooting, and a 'unique' value
+    # if the setup script requires rebooting, and a 'unique' value
     #
     # The unique value is set based on the following conditions:
     # * if the task is set to run on more than one VM, than we assume it can't be shared
@@ -88,7 +88,6 @@ def bucket_tasks(tasks: List[Task]) -> Dict[Tuple, List[Task]]:
             vm,
             pool,
             get_setup_container(task.config),
-            task.config.task.target_exe,
             task.config.task.reboot_after_setup,
             unique,
         )
