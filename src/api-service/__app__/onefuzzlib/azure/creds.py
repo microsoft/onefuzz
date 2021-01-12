c68057b90f83f4f837ed2fe9e44527c6ff448aac
Brian Caswell <bmc@shmoo.com>
2021-01-12 18:19:32 -0500

    don't rely on CLIError

--- src/api-service/__app__/onefuzzlib/azure/creds.py
+++ src/api-service/__app__/onefuzzlib/azure/creds.py
@@ -37,17 +37,10 @@ def get_identity() -> DefaultAzureCredential:
 @cached
 def mgmt_client_factory(client_class: Any) -> Any:
     try:
-        from azure.cli.core import CLIError
         from azure.common.client_factory import get_client_from_cli_profile
 
         return get_client_from_cli_profile(client_class)
-    except CLIError:
-        pass
-    except OSError:
-        pass
-    except ImportError:
-        pass
-    except ModuleNotFoundError:
+    except Exception:
         pass
 
     if issubclass(client_class, SubscriptionClient):
