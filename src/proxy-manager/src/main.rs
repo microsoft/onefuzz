1c1c0d6c0b6ebc65258d3b0732cd6736505ef047
Brian Caswell <bmc@shmoo.com>
2021-02-04 10:12:45 -0500

    always push notify

--- src/proxy-manager/src/main.rs
+++ src/proxy-manager/src/main.rs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 #[macro_use]
-extern crate log;
+extern crate onefuzz_telemetry;
 #[macro_use]
 extern crate anyhow;
 #[macro_use]
@@ -10,31 +10,39 @@ extern crate clap;
 
 mod config;
 mod proxy;
+
 use anyhow::Result;
 use clap::{App, Arg, SubCommand};
 use config::{Config, ProxyError::MissingArg};
-use std::io::Write;
+use std::{
+    io::{stdout, Write},
+    time::Instant,
+};
+use tokio::{
+    runtime::Runtime,
+    time::{delay_for, Duration},
+};
 
-const MINIMUM_NOTIFY_INTERVAL: tokio::time::Duration = std::time::Duration::from_secs(120);
-const POLL_INTERVAL: tokio::time::Duration = tokio::time::Duration::from_secs(5);
+const MINIMUM_NOTIFY_INTERVAL: Duration = Duration::from_secs(120);
+const POLL_INTERVAL: Duration = Duration::from_secs(5);
 
 async fn run(mut proxy_config: Config) -> Result<()> {
-    let mut last_updated = std::time::Instant::now();
+    let mut last_updated = Instant::now();
     loop {
         info!("checking updates");
-        if proxy_config.update().await? {
-            last_updated = std::time::Instant::now();
-        } else if last_updated + MINIMUM_NOTIFY_INTERVAL < std::time::Instant::now() {
+        proxy_config.update().await?;
+
+        if last_updated + MINIMUM_NOTIFY_INTERVAL < Instant::now() {
             proxy_config.notify().await?;
-            last_updated = std::time::Instant::now();
+            last_updated = Instant::now();
         }
 
-        tokio::time::delay_for(POLL_INTERVAL).await;
+        delay_for(POLL_INTERVAL).await;
     }
 }
 
 fn main() -> Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let license_cmd = SubCommand::with_name("licenses").about("display third-party licenses");
 
@@ -57,7 +65,7 @@ fn main() -> Result<()> {
     let matches = app.get_matches();
 
     if matches.subcommand_matches("licenses").is_some() {
-        std::io::stdout().write_all(include_bytes!("../data/licenses.json"))?;
+        stdout().write_all(include_bytes!("../data/licenses.json"))?;
         return Ok(());
     }
 
@@ -68,6 +76,6 @@ fn main() -> Result<()> {
     let proxy = Config::from_file(config_path)?;
 
     info!("parsed initial config");
-    let mut rt = tokio::runtime::Runtime::new()?;
+    let mut rt = Runtime::new()?;
     rt.block_on(run(proxy))
 }
