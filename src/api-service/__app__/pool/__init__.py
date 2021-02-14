6874719fc18b9f4413bd0f5e2860b82ae9f206c1
anslutsk <73055543+anslutsk@users.noreply.github.com>
2021-02-13 21:14:48 -0800

    typo

--- src/api-service/__app__/pool/__init__.py
+++ src/api-service/__app__/pool/__init__.py
@@ -43,8 +43,8 @@ def set_config(pool: Pool) -> Pool:
 
     multi_tenant_domain = os.environ.get("MULTI_TENANT_DOMAIN")
     if multi_tenant_domain:
-        pools.config.multi_tenant_domain = multi_tenant_domain
-        pools.config.onefuzz_url = "https://%s/%s" % (
+        pool.config.multi_tenant_domain = multi_tenant_domain
+        pool.config.onefuzz_url = "https://%s/%s" % (
             multi_tenant_domain,
             os.environ.get("ONEFUZZ_INSTANCE_NAME"),
         )
