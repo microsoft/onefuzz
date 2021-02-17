68a5fd93f56d543c3747b1546436e17962a0eb9f
Cheick Keita <kcheick@gmail.com>
2021-02-16 22:58:31 -0800

    build fix

--- src/proxy-manager/src/config.rs
+++ src/proxy-manager/src/config.rs
@@ -131,7 +131,7 @@ impl Config {
 
     pub async fn notify(&self) -> Result<()> {
         info!("notifying service of proxy update");
-        let client = QueueClient::new(self.data.notification.clone());
+        let client = QueueClient::new(self.data.notification.clone())?;
 
         client
             .enqueue(NotifyResponse {
