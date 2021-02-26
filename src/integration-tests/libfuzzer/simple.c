ae8c52a3ad98b20c374c169705c1907e5ef4673c
Brian Caswell <bmc@shmoo.com>
2021-02-26 11:54:20 -0500

    fix windows example builds

--- src/integration-tests/libfuzzer/simple.c
+++ src/integration-tests/libfuzzer/simple.c
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <stdint.h>
 #include <stdlib.h>
 
 
-int LLVMFuzzerTestOneInput(const u_int8_t *data, size_t len) {
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t len) {
   int cnt = 0;
 
   if (len < 4) {
