bc6c8408c43a27f997e4165fd6e8335f1b2161ff
bmc-msft <41130664+bmc-msft@users.noreply.github.com>
2021-02-26 10:27:51 -0500

    add `onefuzz containers files download_dir` (#598)
    fixes #571

---
+++ src/cli/onefuzz/azcopy.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import subprocess  # nosec
+
+
+def azcopy_sync(src: str, dst: str) -> None:
+    """ Expose azcopy for uploading/downloading files """
+
+    azcopy = os.environ.get("AZCOPY") or shutil.which("azcopy")
+    if not azcopy:
+        raise Exception(
+            "unable to find 'azcopy' in path or AZCOPY environment variable"
+        )
+
+    # security note: callers need to understand the src/dst for this.
+    subprocess.check_output([azcopy, "sync", src, dst])  # nosec
