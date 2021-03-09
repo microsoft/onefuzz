d998e8e32f0afe6943d5a20c36a358e8b6a2bcb4
Noah Harper <noharper@microsoft.com>
2021-03-08 16:23:33 -0800

    Updating context in heartbeat.rs to include job_id

--- src/agent/onefuzz-agent/src/tasks/heartbeat.rs
+++ src/agent/onefuzz-agent/src/tasks/heartbeat.rs
@@ -54,6 +54,7 @@ pub async fn init_task_heartbeat(
             let task_id = context.state.task_id;
             let machine_id = context.state.machine_id;
             let machine_name = context.state.machine_name.clone();
+            let job_id = context.state.job_id;
 
             let data = HeartbeatClient::<TaskContext, _>::drain_current_messages(context.clone());
             let _ = context
