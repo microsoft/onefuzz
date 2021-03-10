60a1120157f900edfaf0ccf04ded491d85dca611
Noah Harper <noharper@microsoft.com>
2021-03-10 12:49:55 -0800

    Adding UserInfo filter function for logging.

--- src/api-service/__app__/onefuzzlib/events.py
+++ src/api-service/__app__/onefuzzlib/events.py
@@ -33,9 +33,53 @@ def get_events() -> Optional[str]:
         return None
 
 
+def log_event(event: Event, event_type: EventType) -> None:
+    
+    clone_event = event.copy(deep=True)
+    log_event_recurs(clone_event)
+    logging.info("sending event: %s - %s", event_type, clone_event)
+    
+
+def log_event_recurs(clone_event: Event, visited: Set[int] = set()) -> Event: 
+
+    if id(clone_event) in visited:
+        return 
+
+    visited.add(id(clone_event))
+
+    for field in clone_event.__fields__:
+        field_data = getattr(clone_event, field)
+        
+        if isinstance(field_data, UserInfo):
+        
+            field_data = None
+
+        elif isinstance(field_data, List):
+            
+            if len(field_data) > 0 and not isinstance(field_data[0], BaseModel):
+                continue 
+            for data in field_data:
+                log_event_recurs(data, visited)
+
+        elif isinstance(field_data, dict):
+            
+            for key in field_data:
+                if not isinstance(field_data[key], BaseModel):
+                    continue
+                log_event_recurs(field_data[key], visited)
+
+        else: 
+
+            if isinstance(field_data, BaseModel):
+                log_event_recurs(field_data, visited)
+        
+        setattr(clone_event, field, field_data)
+
+    return clone_event
+
 def send_event(event: Event) -> None:
     event_type = get_event_type(event)
-    logging.info("sending event: %s - %s", event_type, event)
+    log_event(event, event_type)
     event_message = EventMessage(
         event_type=event_type,
         event=event,
@@ -44,3 +88,6 @@ def send_event(event: Event) -> None:
     )
     EVENTS.put(event_message)
     Webhook.send_event(event_message)
+
+if __name__ == "__main__":
+    print("hello")
