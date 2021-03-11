11ff21c940a51c6336100952cc06f1e695b4bd62
Noah Harper <noharper@microsoft.com>
2021-03-10 17:27:41 -0800

    Getting rid of unused imports

--- src/api-service/__app__/onefuzzlib/events.py
+++ src/api-service/__app__/onefuzzlib/events.py
@@ -6,16 +6,13 @@
 import json
 import logging
 from queue import Empty, Queue
-from typing import (Any, Callable, Dict, List, Mapping, Optional, Set, Tuple,
-                    Type, TypeVar, Union)
-from uuid import UUID, uuid4
+from typing import (List, Optional, Set)
+from uuid import uuid4
 
-from onefuzztypes.enums import OS, Architecture, ContainerType, TaskType
-from onefuzztypes.events import (Event, EventMessage, EventTaskCreated,
+from onefuzztypes.enums import ContainerType
+from onefuzztypes.events import (Event, EventMessage,
                                  EventType, get_event_type)
-from onefuzztypes.models import (SecretData, TaskConfig, TaskContainers,
-                                 TaskDetails, TaskPool, UserInfo)
-from onefuzztypes.primitives import Container, PoolName
+from onefuzztypes.models import UserInfo
 from pydantic import BaseModel, Field
 
 from .azure.creds import get_instance_id, get_instance_name
