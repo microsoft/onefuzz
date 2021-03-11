9e058771ef5b23bb5137e269a56aac443db23236
Brian Caswell <bmc@shmoo.com>
2021-03-11 02:35:27 -0500

    be more quiet

--- src/integration-tests/git-bisect/src/bisect-onefuzz.sh
+++ src/integration-tests/git-bisect/src/bisect-onefuzz.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 PROJECT=${PROJECT:-regression-test}
 TARGET=${TARGET:-$(uuidgen)}
 BUILD=regression-$(git rev-parse HEAD)
 POOL=${ONEFUZZ_POOL:-linux}
 
-echo 'checking build'
-git show -s --format=%s
-
 make clean
 make
 onefuzz template regression libfuzzer ${PROJECT} ${TARGET} ${BUILD} ${POOL} --check_regressions --delete_input_container --reports --crashes $*
-echo 'not this onefuzz job'
