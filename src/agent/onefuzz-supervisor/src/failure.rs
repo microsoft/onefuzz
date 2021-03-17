d978bc27bdd614ac8d9a1827fbb9bc72f46d4654
Brian Caswell <bmc@shmoo.com>
2021-03-17 14:12:44 -0400

    add error message when failing due to pre-existing workset configs

---
+++ src/agent/onefuzz-supervisor/src/failure.rs
@@ -0,0 +1,24 @@
+use anyhow::{Context, Error, Result};
+use onefuzz::fs::onefuzz_root;
+use std::fs;
+use std::path::PathBuf;
+
+const FAILURE_FILE: &str = "onefuzz-supervisor-failure.txt";
+
+pub fn failure_path() -> Result<PathBuf> {
+    Ok(onefuzz_root()?.join(FAILURE_FILE))
+}
+
+pub fn save_failure(err: &Error) -> Result<()> {
+    let path = failure_path()?;
+    let message = format!("{}", err);
+    fs::write(&path, message)
+        .with_context(|| format!("unable to write file: {}", path.display()))?;
+    Ok(())
+}
+
+pub fn read_failure() -> Result<String> {
+    let path = failure_path()?;
+    Ok(fs::read_to_string(&path)
+        .with_context(|| format!("unable to read failure: {}", path.display()))?)
+}
