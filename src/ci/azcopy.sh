b8b76ced50840029fd93807afe752d5705b073ee
Brian Caswell <bmc@shmoo.com>
2021-03-23 02:27:52 -0400

    hardcode wget to use TLSv1.2 per aka.ms team to work around failures

--- src/ci/azcopy.sh
+++ src/ci/azcopy.sh
@@ -7,10 +7,10 @@ set -ex
 
 mkdir -p artifacts/azcopy
 
-wget --retry-connrefused -t 30 --waitretry=5 -O azcopy.zip https://aka.ms/downloadazcopy-v10-windows
+wget --secure-protocol=TLSv1_2 --retry-connrefused -t 30 --waitretry=5 -O azcopy.zip https://aka.ms/downloadazcopy-v10-windows
 unzip azcopy.zip
 mv azcopy_windows*/* artifacts/azcopy/
 
-wget --retry-connrefused -t 30 --waitretry=5 -O azcopy.tgz https://aka.ms/downloadazcopy-v10-linux
+wget --secure-protocol=TLSv1_2 --retry-connrefused -t 30 --waitretry=5 -O azcopy.tgz https://aka.ms/downloadazcopy-v10-linux
 tar zxvf azcopy.tgz
 mv azcopy_linux_amd64*/* artifacts/azcopy/
