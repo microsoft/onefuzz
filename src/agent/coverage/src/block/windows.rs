bd41623633b3e9e0909630f92febf54e7dc7d3fb
Joe Ranweiler <joranwei@microsoft.com>
2021-03-30 22:52:49 +0000

    Remove redundant try

--- src/agent/coverage/src/block/windows.rs
+++ src/agent/coverage/src/block/windows.rs
@@ -148,8 +148,7 @@ impl<'a> Recorder<'a> {
                 }
             }
 
-            self.coverage
-                .increment(breakpoint.module, breakpoint.offset)?;
+            self.coverage.increment(breakpoint.module, breakpoint.offset);
         } else {
             let pc = if let Ok(pc) = dbg.read_program_counter() {
                 format!("{:x}", pc)
