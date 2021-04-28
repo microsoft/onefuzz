bd8153fcf24769714d044c839a9768381f9c90c8
Joe Ranweiler <joranwei@microsoft.com>
2021-04-28 16:06:32 +0000

    Add test for generic coverage report

--- src/agent/coverage/src/report.rs
+++ src/agent/coverage/src/report.rs
@@ -42,3 +42,158 @@ pub struct CoverageReportEntry<C, M> {
     #[serde(flatten)]
     pub(crate) coverage: C,
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+    use serde_json::json;
+
+    use crate::test::module_path;
+
+    use super::*;
+
+    #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
+    struct Metadata {
+        checksum: String,
+        pid: u64,
+    }
+
+    #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
+    struct Edge {
+        edges: Vec<EdgeCov>,
+    }
+
+    #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
+    struct EdgeCov {
+        src: u32,
+        dst: u32,
+        count: u32,
+    }
+
+    // Example of using `CoverageReport` for alternative coverage types.
+    type EdgeCoverageReport = CoverageReport<Edge, Metadata>;
+
+    #[test]
+    fn test_coverage_report() -> Result<()> {
+        let main_exe = module_path("/onefuzz/main.exe")?;
+        let some_dll = module_path("/common/some.dll")?;
+
+        let text = serde_json::to_string(&json!([
+            {
+                "module": some_dll,
+                "checksum": "5feceb66",
+                "pid": 123,
+                "edges": [
+                    { "src": 10, "dst": 20, "count": 0 },
+                    { "src": 10, "dst": 30, "count": 1 },
+                    { "src": 30, "dst": 40, "count": 1 },
+                ],
+            },
+            {
+                "module": some_dll,
+                "checksum": "ffc86f38",
+                "pid": 456,
+                "edges": [
+                    { "src": 100, "dst": 200, "count": 1 },
+                    { "src": 200, "dst": 300, "count": 0 },
+                    { "src": 300, "dst": 400, "count": 0 },
+                ],
+            },
+            {
+                "module": main_exe,
+                "checksum": "d952786c",
+                "pid": 123,
+                "edges": [
+                    { "src": 1000, "dst": 2000, "count": 1 },
+                    { "src": 2000, "dst": 3000, "count": 0 },
+                ],
+            },
+        ]))?;
+
+        let report = EdgeCoverageReport {
+            entries: vec![
+                CoverageReportEntry {
+                    module: some_dll.to_string(),
+                    metadata: Metadata {
+                        checksum: "5feceb66".into(),
+                        pid: 123,
+                    },
+                    coverage: Edge {
+                        edges: vec![
+                            EdgeCov {
+                                src: 10,
+                                dst: 20,
+                                count: 0,
+                            },
+                            EdgeCov {
+                                src: 10,
+                                dst: 30,
+                                count: 1,
+                            },
+                            EdgeCov {
+                                src: 30,
+                                dst: 40,
+                                count: 1,
+                            },
+                        ],
+                    },
+                },
+                CoverageReportEntry {
+                    module: some_dll.to_string(),
+                    metadata: Metadata {
+                        checksum: "ffc86f38".into(),
+                        pid: 456,
+                    },
+                    coverage: Edge {
+                        edges: vec![
+                            EdgeCov {
+                                src: 100,
+                                dst: 200,
+                                count: 1,
+                            },
+                            EdgeCov {
+                                src: 200,
+                                dst: 300,
+                                count: 0,
+                            },
+                            EdgeCov {
+                                src: 300,
+                                dst: 400,
+                                count: 0,
+                            },
+                        ],
+                    },
+                },
+                CoverageReportEntry {
+                    module: main_exe.to_string(),
+                    metadata: Metadata {
+                        checksum: "d952786c".into(),
+                        pid: 123,
+                    },
+                    coverage: Edge {
+                        edges: vec![
+                            EdgeCov {
+                                src: 1000,
+                                dst: 2000,
+                                count: 1,
+                            },
+                            EdgeCov {
+                                src: 2000,
+                                dst: 3000,
+                                count: 0,
+                            },
+                        ],
+                    },
+                },
+            ],
+        };
+
+        let ser = serde_json::to_string(&report)?;
+        assert_eq!(ser, text);
+
+        let de: EdgeCoverageReport = serde_json::from_str(&text)?;
+        assert_eq!(de, report);
+
+        Ok(())
+    }
+}
