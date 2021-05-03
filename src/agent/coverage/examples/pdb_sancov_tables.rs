787dbac0c9176e2a3c8fa3f1f0f06ab56839d3f6
Joe Ranweiler <joranwei@microsoft.com>
2021-05-02 18:08:25 -0700

    Fix imports

--- src/agent/coverage/examples/pdb_sancov_tables.rs
+++ src/agent/coverage/examples/pdb_sancov_tables.rs
@@ -1,12 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use std::fs;
-
 use anyhow::Result;
-use coverage::block::pe_provider::PeSancovBasicBlockProvider;
-use goblin::pe::PE;
-use pdb::PDB;
 use structopt::StructOpt;
 
 #[derive(Debug, PartialEq, StructOpt)]
@@ -20,16 +15,20 @@ struct Opt {
 
 #[cfg(target_os = "windows")]
 fn main() -> Result<()> {
+    use coverage::block::pe_provider::PeSancovBasicBlockProvider;
+    use goblin::pe::PE;
+    use pdb::PDB;
+
     let opt = Opt::from_args();
 
-    let data = fs::read(&opt.pe)?;
+    let data = std::fs::read(&opt.pe)?;
     let pe = PE::parse(&data)?;
 
     let pdb = opt
         .pdb
         .clone()
         .unwrap_or_else(|| opt.pe.with_extension("pdb"));
-    let pdb = fs::File::open(pdb)?;
+    let pdb = std::fs::File::open(pdb)?;
     let mut pdb = PDB::open(pdb)?;
 
     let mut provider = PeSancovBasicBlockProvider::new(&data, &pe, &mut pdb);
