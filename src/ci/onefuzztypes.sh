5fb226c44d9735f7b6bfb0eaf89bf72f9b97ca84
Noah Harper <noharper@microsoft.com>
2021-05-11 17:50:54 -0700

    Fixing generate docs and shell script.

--- src/ci/onefuzztypes.sh
+++ src/ci/onefuzztypes.sh
@@ -25,7 +25,7 @@ echo 'verify webhook docs are up-to-date'
 python -m venv build-docs
 . build-docs/bin/activate
 pip install -e .
-python extra/generate-docs.py > ../../docs/webhook_events.md
-git diff --quiet ../../docs/webhook_events.md
+python extra/generate-docs.py -o "../../docs/"
+git diff ../../docs/webhook_events.md
 deactivate
 rm -rf build-docs
