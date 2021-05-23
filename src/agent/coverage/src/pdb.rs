538231cbe9a87aae22d2dcdaeb198a1cd4b5c7bc
Joe Ranweiler <joranwei@microsoft.com>
2021-05-23 11:56:11 -0700

    Distinguish search errors and absent files

--- src/agent/coverage/src/pdb.rs
+++ src/agent/coverage/src/pdb.rs
@@ -24,7 +24,11 @@ use winapi::um::{dbghelp::SYMOPT_EXACT_SYMBOLS, winnt::HANDLE};
 // See: https://docs.microsoft.com/en-us/windows/win32/api/dbghelp/nf-dbghelp-syminitializew
 const PSEUDO_HANDLE: HANDLE = -2i64 as _;
 
-pub fn find_pdb_path(pe_path: &Path, pe: &PE, target_handle: Option<HANDLE>) -> Result<PathBuf> {
+pub fn find_pdb_path(
+    pe_path: &Path,
+    pe: &PE,
+    target_handle: Option<HANDLE>,
+) -> Result<Option<PathBuf>> {
     let cv = if let Some(DebugData {
         image_debug_directory: _,
         codeview_pdb70_debug_info: Some(cv),
@@ -44,7 +48,7 @@ pub fn find_pdb_path(pe_path: &Path, pe: &PE, target_handle: Option<HANDLE>) -> 
     // If the PE-specified PDB file exists on disk, use that.
     if let Ok(metadata) = fs::metadata(&cv_filename) {
         if metadata.is_file() {
-            return Ok(cv_filename.to_owned());
+            return Ok(Some(cv_filename.to_owned()));
         }
     }
 
