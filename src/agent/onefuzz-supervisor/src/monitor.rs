7ef9aeba017c7cf7bec188cd224f091135146a0f
Brian Caswell <bmc@shmoo.com>
2021-06-15 16:48:56 -0400

    disable truncation

--- src/agent/onefuzz-supervisor/src/monitor.rs
+++ src/agent/onefuzz-supervisor/src/monitor.rs
@@ -9,7 +9,7 @@ pub async fn monitor_processes() -> Result<()> {
     loop {
         let mut processes = processes()?;
         processes.sort_by(|x, y| y.memory_kb.cmp(&x.memory_kb));
-        processes.truncate(10);
+        // processes.truncate(10);
         for process in processes {
             info!("process info {:?}", process)
         }
