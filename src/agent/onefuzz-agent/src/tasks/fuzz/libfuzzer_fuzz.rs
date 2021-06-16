9cb7e037e1f0a0de0aee209d2d10844554c9c4d1
Brian Caswell <bmc@shmoo.com>
2021-06-16 15:33:22 -0400

    clippy

--- src/agent/onefuzz-agent/src/tasks/fuzz/libfuzzer_fuzz.rs
+++ src/agent/onefuzz-agent/src/tasks/fuzz/libfuzzer_fuzz.rs
@@ -109,8 +109,9 @@ impl LibFuzzerFuzzTask {
         let (stats_sender, stats_receiver) = mpsc::unbounded_channel();
         let report_stats = report_runtime_stats(stats_receiver, hb_client);
         let fuzzers = self.run_fuzzers(Some(&stats_sender));
-        futures::try_join!(resync, new_inputs, new_crashes, fuzzers, report_stats)
-            .with_context(|| get_stats().unwrap_or(String::from("unable to get process stats")))?;
+        futures::try_join!(resync, new_inputs, new_crashes, fuzzers, report_stats).with_context(
+            || get_stats().unwrap_or_else(|_| String::from("unable to get process stats")),
+        )?;
 
         Ok(())
     }
