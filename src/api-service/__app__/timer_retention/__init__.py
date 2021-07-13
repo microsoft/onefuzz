54294438677a96c1762e4f27106fc29a7e3cc584
Noah Harper <noharper@microsoft.com>
2021-07-13 16:24:15 -0700

    Working function timer. Added notification and repro table scrubs.

--- src/api-service/__app__/timer_retention/__init__.py
+++ src/api-service/__app__/timer_retention/__init__.py
@@ -6,46 +6,87 @@ import azure.functions as func
 from ..onefuzzlib.events import get_events
 from ..onefuzzlib.jobs import Job
 from ..onefuzzlib.notifications.main import Notification
+from ..onefuzzlib.repro import Repro
 from ..onefuzzlib.tasks.main import Task
 
-RETENTION_POLICY = datetime.timedelta(days=(18))
+RETENTION_POLICY = datetime.timedelta(minutes=(18))
 
 
 def main(mytimer: func.TimerRequest, dashboard: func.Out[str]) -> None:  # noqa: F841
 
-    for job in Job.search():
-        logging.info("Retention Timer Job Search")
-        job_timestamp = job.timestamp
-        if job_timestamp is not None and job_timestamp < (
-            datetime.datetime.now(tz=datetime.timezone.utc) - RETENTION_POLICY
-        ):
-            if job.user_info is not None:
-                logging.info(
-                    "Found job %s older than 18 months. Scrubbing user_info.",
-                    job.job_id,
-                )
-                job.user_info.upn = "noreply@microsoft.com"
-                job.save()
+    time_retained = datetime.datetime.now(tz=datetime.timezone.utc) - RETENTION_POLICY
 
-    for task in Task.search():
-        logging.info("Retention Timer Task Search")
-        task_timestamp = task.timestamp
-        if task_timestamp is not None and task_timestamp < (
-            datetime.datetime.now(tz=datetime.timezone.utc) - RETENTION_POLICY
-        ):
-            if task.user_info is not None:
-                logging.info(
-                    "Found task %s older than 18 months. Scrubbing user_info.",
-                    task.task_id,
-                )
-                task.user_info.upn = "noreply@microsoft.com"
-                task.save()
+    time_filter = f"Timestamp lt datetime'{time_retained.isoformat()}'"
 
-    for notification in Notification.search():
+    # You have to do notification before task, because editing the upn for tasks will change the timestamp
+    for notification in Notification.search(raw_unchecked_filter=time_filter):
         logging.info("Retention Timer Notification Search")
-        logging.info(notification)
+        logging.info(
+            "Found notification %s older than 18 months. Checking related tasks.",
+            notification.notification_id,
+        )
+        container = notification.container
+        task_list = []
+        timestamp_list = []
+        logging.info("Notification Container %s", container)
+        for task in Task.search():
+            # logging.info(task.config)
+            # logging.info(task.config.containers)
+            # logging.info(type(task.config.containers))
+            # logging.info(str(task.config.containers))
+            container_str = str(task.config.containers)
+            # logging.info("break")
+            # logging.info("Container List: %s", container_str)
+            # logging.info((container in container_str))
+            if container in container_str:
+                logging.info("Container List: %s", container_str)
+                task_list.append(task.task_id)
+                timestamp_list.append(task.timestamp)
+        logging.info("Task List")
+        logging.info(task_list)
+        logging.info("Timestamp List")
+        logging.info(timestamp_list)
+        now = datetime.datetime.now(tz=datetime.timezone.utc)
+        if len(timestamp_list) != 0:
+            youngest = max(dt for dt in timestamp_list if dt < now)
+            if youngest < time_retained:
+                logging.info("Deleting Notification")
+                notification.delete()
+            # logging.info(task.config.containers.name)
         # logging.info(notification.config.ado_fields["System.AssignedTo"])
-        logging.info(notification.timestamp)
+        # logging.info(notification)
+
+    for job in Job.search(raw_unchecked_filter=time_filter):
+        logging.info("Retention Timer Job Search")
+        if job.user_info is not None:
+            # logging.info(
+            #     "Found job %s older than 18 months. Scrubbing user_info.",
+            #     job.job_id,
+            # )
+            job.user_info.upn = "None"
+            job.save()
+
+    for task in Task.search(raw_unchecked_filter=time_filter):
+        logging.info("Retention Timer Task Search")
+        if task.user_info is not None:
+            # logging.info(
+            #     "Found task %s older than 18 months. Scrubbing user_info.",
+            #     task.task_id,
+            # )
+            task.user_info.upn = "None"
+            task.save()
+
+    for repro in Repro.search(raw_unchecked_filter=time_filter):
+        logging.info("Retention Timer Repro Search")
+        logging.info(repro)
+        if repro.user_info is not None:
+            logging.info(
+                "Found repro entry for task %s on node %s that is older than 18 months. Scrubbing user_info.",
+                repro.task_id,
+                repro.vm_id,
+            )
+            repro.user_info.upn = "None"
+            repro.save()
 
     events = get_events()
     if events:
