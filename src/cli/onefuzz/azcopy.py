07572f9d9f89825d95c7ebb000b1126dc85d4e3e
Brian Caswell <bmc@shmoo.com>
2021-07-13 14:19:39 -0400

    make invalid AZCOPY environment variables more clear

--- src/cli/onefuzz/azcopy.py
+++ src/cli/onefuzz/azcopy.py
@@ -2,15 +2,26 @@ import os
 import shutil
 import subprocess  # nosec
 
+def find_azcopy() -> str:
+    azcopy = os.environ.get("AZCOPY")
 
-def azcopy_sync(src: str, dst: str) -> None:
-    """Expose azcopy for uploading/downloading files"""
+    if azcopy:
+        if not os.path.exists(azcopy):
+            raise Exception(f"AZCOPY environment variable is invalid: {azcopy}")
+    else:
+        azcopy = shutil.which("azcopy")
 
-    azcopy = os.environ.get("AZCOPY") or shutil.which("azcopy")
     if not azcopy:
         raise Exception(
             "unable to find 'azcopy' in path or AZCOPY environment variable"
         )
+
+    return azcopy
+
+def azcopy_sync(src: str, dst: str) -> None:
+    """Expose azcopy for uploading/downloading files"""
+
+    azcopy = find_azcopy()
 
     # security note: callers need to understand the src/dst for this.
     subprocess.check_output([azcopy, "sync", src, dst, "--recursive=true"])  # nosec
