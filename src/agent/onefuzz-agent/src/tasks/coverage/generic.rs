cbeff004d3f66f21fa879032c7535e78cff9acec
bmc-msft <41130664+bmc-msft@users.noreply.github.com>
2021-07-15 18:45:38 -0400

    Apply suggestions from code review
    Co-authored-by: Joe Ranweiler <joe@lemma.co>

--- src/agent/onefuzz-agent/src/tasks/coverage/generic.rs
+++ src/agent/onefuzz-agent/src/tasks/coverage/generic.rs
@@ -286,8 +286,8 @@ impl<'a> TaskContext<'a> {
                     if entry.file_type().await?.is_file() {
                         self.record_input(&entry.path()).await?;
                         count += 1;
-                        // make sure we save & sync coverage every 10 inputs
 
+                        // make sure we save & sync coverage every 10 inputs
                         if count % 10 == 0 {
                             self.save_and_sync_coverage().await?;
                         }
