203d48dca08f09c18b25d8b2ead8780235b32b52
Brian Caswell <bmc@shmoo.com>
2021-08-10 16:25:03 -0400

    handle call_if* not supporting additional argument callbacks

--- src/api-service/__app__/negotiate/__init__.py
+++ src/api-service/__app__/negotiate/__init__.py
@@ -5,6 +5,8 @@
 
 import azure.functions as func
 
+from ..onefuzzlib.endpoint_authorization import call_if_user
+
 # This endpoint handles the signalr negotation
 # As we do not differentiate from clients at this time, we pass the Functions runtime
 # provided connection straight to the client
@@ -13,18 +15,20 @@ import azure.functions as func
 # https://docs.microsoft.com/en-us/azure/azure-signalr/signalr-concept-internals
 
 
+def main(req: func.HttpRequest, connectionInfoJson: str) -> func.HttpResponse:
+    # NOTE: this is a sub-method because the call_if* do not support callbacks with
+    # additional arguments at this time.  Once call_if* supports additional arguments,
+    # this should be made a generic function
+    def post(req: func.HttpRequest) -> func.HttpResponse:
+        return func.HttpResponse(
+            connectionInfoJson,
+            status_code=200,
+            headers={"Content-type": "application/json"},
+        )
 
-def post(req: func.HttpRequest, connectionInfoJson: str) -> func.HttpResponse:
-    return func.HttpResponse(
-        connectionInfoJson,
-        status_code=200,
-        headers={"Content-type": "application/json"},
-    )
-
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
     methods = {"POST": post}
     method = methods[req.method]
+
     result = call_if_user(req, method)
 
     return result
