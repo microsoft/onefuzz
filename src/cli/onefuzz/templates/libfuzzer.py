67b64a53c2ddb770088da52d4a78df044c6e9184
Jordyn Puryear <jordynp@microsoft.com>
2021-09-15 14:03:41 -0700

    Including readonly_inputs container in the fuzzing task creation

--- src/cli/onefuzz/templates/libfuzzer.py
+++ src/cli/onefuzz/templates/libfuzzer.py
@@ -105,6 +105,7 @@ class Libfuzzer(Command):
             (ContainerType.setup, containers[ContainerType.setup]),
             (ContainerType.crashes, containers[ContainerType.crashes]),
             (ContainerType.inputs, containers[ContainerType.inputs]),
+            (ContainerType.readonly_inputs, containers[ContainerType.readonly_inputs])
         ]
         self.logger.info("creating libfuzzer task")
 
