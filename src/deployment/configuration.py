734838b27ca8e326b671efabb70b4111addbd992
nharper285 <nharper285@gmail.com>
2021-10-11 09:49:29 -0700

    removing import

--- src/deployment/configuration.py
+++ src/deployment/configuration.py
@@ -10,7 +10,6 @@ from uuid import UUID
 
 from azure.common.client_factory import get_client_from_cli_profile
 from azure.cosmosdb.table.tableservice import TableService
-from azure.mgmt.storage import StorageManagementClient
 from onefuzztypes.models import NetworkSecurityGroupConfig
 
 storage_client_logger = logging.getLogger("azure.cosmosdb.table.common.storageclient")
