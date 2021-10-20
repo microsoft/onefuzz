398a39c69ec211b89c0a8c8910ac6a84b91502dc
nharper285 <nharper285@gmail.com>
2021-10-20 10:40:41 -0700

    Fix set_allowed call

--- src/api-service/__app__/onefuzzlib/azure/nsg.py
+++ src/api-service/__app__/onefuzzlib/azure/nsg.py
@@ -182,7 +182,7 @@ def set_allowed(name: str, sources: NetworkSecurityGroupConfig) -> Union[None, E
 
 
 def clear_all_rules(name: str) -> Union[None, Error]:
-    return set_allowed(name, [])
+    return set_allowed(name, NetworkSecurityGroupConfig())
 
 
 def get_all_rules(name: str) -> Union[Error, List[SecurityRule]]:
