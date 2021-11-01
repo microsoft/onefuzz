a907f068a0e1241fd718df0c47f6d572bb18c122
nharper285 <noharper@microsoft.com>
2021-11-01 15:17:47 -0700

    Merge fix

---
+++ src/deployment/deploylib/tests/README.md
@@ -0,0 +1,7 @@
+# Deployment tests.
+
+- `test_deploy_config.py` 
+
+    Requires the deployment packages and scripts from OneFuzz.
+
+    `test_deploy_config.py` validates that the Deploy script takes properly formatted json configuration..
---
+++ src/deployment/deploylib/tests/test_deploy_config.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import unittest
+from typing import Any, List
+
+from deploylib.configuration import NetworkSecurityConfig
+
+
+class TestNetworkSecurityConfig:
+    allowed_ips: List[str]
+    allowed_service_tags: List[str]
+
+
+class DeployTests(unittest.TestCase):
+    def test_config(self) -> None:
+        ## Test Invalid Configs
+        #  Test Dictionary
+        invalid_config: Any = ""
+        with self.assertRaises(Exception):
+            NetworkSecurityConfig(invalid_config)
+        #  Test Empty Dic
+        invalid_config = {}
+        with self.assertRaises(Exception):
+            NetworkSecurityConfig(invalid_config)
+        #  Test Invalid Outer Keys
+        invalid_config = {"": ""}
+        with self.assertRaises(Exception):
+            NetworkSecurityConfig(invalid_config)
+        #  Test Inner Dictionary
+        invalid_config = {"proxy_nsg_config": ""}
+        with self.assertRaises(Exception):
+            NetworkSecurityConfig(invalid_config)
+        #  Test Inner Keys
+        invalid_config = {"proxy_nsg_config": {}}
+        with self.assertRaises(Exception):
+            NetworkSecurityConfig(invalid_config)
+        #  Test Inner Keys
+        invalid_config = {"proxy_nsg_config": {"allowed_ips": ""}}
+        with self.assertRaises(Exception):
+            NetworkSecurityConfig(invalid_config)
+        #  Test Inner Dict Values (lists)
+        invalid_config = {
+            "proxy_nsg_config": {"allowed_ips": [], "allowed_service_tags": ""}
+        }
+        with self.assertRaises(Exception):
+            NetworkSecurityConfig(invalid_config)
+        #  Test List Values
+        invalid_config = {
+            "proxy_nsg_config": {
+                "allowed_ips": [1, 2],
+                "allowed_service_tags": ["10.0.0.0"],
+            }
+        }
+        with self.assertRaises(Exception):
+            NetworkSecurityConfig(invalid_config)
+
+        ## Test Valid Configs
+        #  Test Empty Lists
+        valid_config: Any = {
+            "proxy_nsg_config": {"allowed_ips": [], "allowed_service_tags": []}
+        }
+        NetworkSecurityConfig(valid_config)
+        #  Test Wild Card Lists
+        valid_config = {
+            "proxy_nsg_config": {"allowed_ips": ["*"], "allowed_service_tags": []}
+        }
+        NetworkSecurityConfig(valid_config)
+        #  Test IPs Lists
+        valid_config = {
+            "proxy_nsg_config": {
+                "allowed_ips": ["10.0.0.1", "10.0.0.2"],
+                "allowed_service_tags": [],
+            }
+        }
+        NetworkSecurityConfig(valid_config)
+        #  Test Tags Lists
+        valid_config = {
+            "proxy_nsg_config": {
+                "allowed_ips": ["10.0.0.1", "10.0.0.2"],
+                "allowed_service_tags": ["Internet"],
+            }
+        }
+        NetworkSecurityConfig(valid_config)
+
+
+if __name__ == "__main__":
+    unittest.main()
