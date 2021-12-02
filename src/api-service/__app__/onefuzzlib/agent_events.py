1c1f79c6622bafdb3f1f5289014d0299fdf2e128
Cheick Keita <kcheick@gmail.com>
2021-12-01 19:03:43 -0800

    ignore done message when the node is deleted

--- src/api-service/__app__/onefuzzlib/agent_events.py
+++ src/api-service/__app__/onefuzzlib/agent_events.py
@@ -46,6 +46,11 @@ def on_state_update(
     state = state_update.state
     node = get_node(machine_id)
     if isinstance(node, Error):
+        if state == NodeState.done:
+            logging.warning(
+                f"unable to process state update event. machine_id:{machine_id} envelope:{state_update} error:{node}"
+            )
+            return None
         return node
 
     if state == NodeState.free:
