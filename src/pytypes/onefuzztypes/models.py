10db1597b9a9d49bfcc6d1e02e938c20dab297a1
nharper285 <nharper285@gmail.com>
2022-01-26 14:38:33 -0800

    Formatting.

--- src/pytypes/onefuzztypes/models.py
+++ src/pytypes/onefuzztypes/models.py
@@ -570,7 +570,7 @@ class Node(BaseModel):
     timestamp: Optional[datetime] = Field(alias="Timestamp")
 
     # Set only once, when a node is initialized.
-    initialized_at: Optional[datetime]  
+    initialized_at: Optional[datetime]
     pool_name: PoolName
     pool_id: Optional[UUID]
     machine_id: UUID
