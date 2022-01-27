6f92223e2b68164436fe818e3110a1871f550acf
Joe Ranweiler <joranwei@microsoft.com>
2022-01-27 11:30:57 -0800

    Default `setup_dir` to target exe dir

--- src/agent/onefuzz/examples/test-input.rs
+++ src/agent/onefuzz/examples/test-input.rs
@@ -16,7 +16,7 @@ struct Opt {
     pub options: Vec<String>,
 
     #[structopt(short, long)]
-    pub setup_dir: PathBuf,
+    pub setup_dir: Option<PathBuf>,
 
     #[structopt(short, long)]
     pub input: PathBuf,
@@ -38,8 +38,13 @@ struct Opt {
 async fn main() -> Result<()> {
     let opt = Opt::from_args();
 
+    // Default `setup_dir` to base dir of
+    let setup_dir = opt.setup_dir
+        .clone()
+        .unwrap_or_else(|| opt.exe.parent().expect("target exe missing file component").to_owned());
+
     let env = Default::default();
-    let tester = Tester::new(&opt.setup_dir, &opt.exe, &opt.options, &env);
+    let tester = Tester::new(&setup_dir, &opt.exe, &opt.options, &env);
 
     let check_debugger = !opt.no_check_debugger;
     let tester = tester
