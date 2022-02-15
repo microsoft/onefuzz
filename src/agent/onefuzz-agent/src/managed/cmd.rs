837268b6345e6a489b202129d3ef782f2b669595
Joe Ranweiler <joranwei@microsoft.com>
2022-02-15 20:14:42 +0000

    Check for impending OOM in managed task worker

--- src/agent/onefuzz-agent/src/managed/cmd.rs
+++ src/agent/onefuzz-agent/src/managed/cmd.rs
@@ -5,6 +5,12 @@ use crate::tasks::config::{CommonConfig, Config};
 use anyhow::Result;
 use clap::{App, Arg, SubCommand};
 use std::path::PathBuf;
+use std::time::Duration;
+
+// 100 MB.
+const MIN_AVAILABLE_BYTES: u64 = 100 * 1_000_000;
+
+const OOM_CHECK_INTERVAL: Duration = Duration::from_secs(5);
 
 pub async fn run(args: &clap::ArgMatches<'_>) -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
@@ -13,7 +19,17 @@ pub async fn run(args: &clap::ArgMatches<'_>) -> Result<()> {
     let config = Config::from_file(config_path, setup_dir)?;
 
     init_telemetry(config.common());
-    let result = config.run().await;
+
+    let result = tokio::select! {
+        result = config.run() => result,
+
+        // Ignore this task if it returns due to a querying error.
+        Ok(oom) = out_of_memory(MIN_AVAILABLE_BYTES) => {
+            // Convert the OOM notification to an error, so we can log it below.
+            let err = format_err!("out of memory: {} bytes available, {} required", oom.available_bytes, oom.min_bytes);
+            Err(err)
+        },
+    };
 
     if let Err(err) = &result {
         error!("error running task: {:?}", err);
@@ -21,6 +37,35 @@ pub async fn run(args: &clap::ArgMatches<'_>) -> Result<()> {
 
     onefuzz_telemetry::try_flush_and_close();
     result
+}
+
+// Periodically check available system memory.
+//
+// If available memory drops below the minimum, exit informatively.
+async fn out_of_memory(min_bytes: u64) -> Result<OutOfMemory> {
+    loop {
+        match onefuzz::memory::available_bytes() {
+            Ok(available_bytes) => {
+                if available_bytes < min_bytes {
+                    return Ok(OutOfMemory {
+                        available_bytes,
+                        min_bytes,
+                    });
+                }
+            }
+            Err(err) => {
+                warn!("error querying system memory usage: {}", err);
+                return Err(err);
+            }
+        }
+
+        tokio::time::sleep(OOM_CHECK_INTERVAL).await;
+    }
+}
+
+struct OutOfMemory {
+    available_bytes: u64,
+    min_bytes: u64,
 }
 
 fn init_telemetry(config: &CommonConfig) {
