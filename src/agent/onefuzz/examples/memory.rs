bceb44992fa33203adfdf6e6a22758bde502af76
Joe Ranweiler <joranwei@microsoft.com>
2022-02-16 16:58:16 +0000

    Fix example for macOS

--- src/agent/onefuzz/examples/memory.rs
+++ src/agent/onefuzz/examples/memory.rs
@@ -1,5 +1,11 @@
+#[cfg(not(target_os = "macos"))]
 fn main() {
     let bytes = onefuzz::memory::available_bytes().unwrap();
     let gb = (bytes as f64) * 1e-9;
     println!("available bytes: {} ({:.1} GB)", bytes, gb);
+}
+
+#[cfg(target_os = "macos")]
+fn main() {
+    unimplemented!()
 }
