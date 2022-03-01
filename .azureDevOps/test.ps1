c7546018ba8adaf6b79022253ac83ddf025b9458
Cheick Keita <kcheick@gmail.com>
2022-03-01 14:55:42 -0800

    remove file

--- .azureDevOps/test.ps1
+++
@@ -1,8 +0,0 @@
-Install-Module -Name PowerShellForGitHub
-
-
-$secureString = ("<Your Access Token>" | ConvertTo-SecureString -AsPlainText -Force)
-$cred = New-Object System.Management.Automation.PSCredential "username is ignored", $secureString
-Set-GitHubAuthentication -Credential $cred
-
-
