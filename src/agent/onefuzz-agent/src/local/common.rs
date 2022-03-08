d59b5985edf57e9074af1f66443aec50931dfe02
Hayley Call <Hayley.Call@microsoft.com>
2022-03-08 14:27:37 -0800

    Improved backoff update changes

--- src/agent/onefuzz-agent/src/local/common.rs
+++ src/agent/onefuzz-agent/src/local/common.rs
@@ -299,10 +299,8 @@ pub async fn wait_for_dir(path: impl AsRef<Path>) -> Result<()> {
         if path.as_ref().exists() {
             Ok(())
         } else {
-            let max_retry_period: Option<Duration> = Some(WAIT_FOR_MAX_WAIT);
             Err(BackoffError::Transient {
-                err: anyhow::anyhow!("path '{:?}' does not exist", path.as_ref()),
-                retry_after: max_retry_period,
+                err: anyhow::anyhow!("path '{}' does not exist", path.as_ref().display()),
             })
         }
     };
