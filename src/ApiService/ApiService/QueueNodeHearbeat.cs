f11e79de4bd067358fb0f12e412bcb60f20fbbdc
Stas <stishkin@live.com>
2022-04-01 13:20:08 -0700

    Starting work... - add http client - add package lock - set namespace to Microsoft.OneFuzz.Service - add static class to get environment variables (#1736)
    Co-authored-by: stas <statis@microsoft.com>

--- src/ApiService/ApiService/QueueNodeHearbeat.cs
+++ src/ApiService/ApiService/QueueNodeHearbeat.cs
@@ -7,59 +7,55 @@ using Azure.Data.Tables;
 using System.Threading.Tasks;
 using Azure;
 
+namespace Microsoft.OneFuzz.Service;
 
-namespace ApiService
+
+enum HeartbeatType
+{
+    MachineAlive,
+    TaskAlive,
+}
+
+record NodeHeartbeatEntry(string NodeId, Dictionary<string, HeartbeatType>[] data);
+
+
+public class QueueNodeHearbeat
 {
 
-   
-
-    enum HeartbeatType
+    private (string, string) GetStorageAccountNameAndKey(string? accountId)
     {
-        MachineAlive,
-        TaskAlive,
+        //TableEntity
+        return ("test", "test");
     }
 
-    record NodeHeartbeatEntry(string NodeId, Dictionary<string, HeartbeatType>[] data);
-
-
-    public class QueueNodeHearbeat
+    private async Task<TableServiceClient> GetStorageClient(string? table, string? accounId)
     {
-
-        private (string, string) GetStorageAccountNameAndKey(string? accountId)
+        accounId ??= System.Environment.GetEnvironmentVariable("ONEFUZZ_FUNC_STORAGE");
+        if (accounId == null)
         {
-            //TableEntity
-            return ("test", "test");
+            throw new Exception("ONEFUZZ_FUNC_STORAGE environment variable not set");
         }
+        var (name, key) = GetStorageAccountNameAndKey(accounId);
+        var tableClient = new TableServiceClient(new Uri(accounId), new TableSharedKeyCredential(name, key));
+        await tableClient.CreateTableIfNotExistsAsync(table);
+        return tableClient;
+    }
 
-        private async Task<TableServiceClient> GetStorageClient(string? table, string? accounId)
-        {
-            accounId ??= System.Environment.GetEnvironmentVariable("ONEFUZZ_FUNC_STORAGE");
-            if (accounId == null)
-            {
-                throw new Exception("ONEFUZZ_FUNC_STORAGE environment variable not set");
-            }
-            var (name, key) = GetStorageAccountNameAndKey(accounId);
-            var tableClient = new TableServiceClient(new Uri(accounId), new TableSharedKeyCredential(name, key));
-            await tableClient.CreateTableIfNotExistsAsync(table);
-            return tableClient;
-        }
+    private readonly ILogger _logger;
 
-        private readonly ILogger _logger;
+    public QueueNodeHearbeat(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<QueueNodeHearbeat>();
+    }
 
-        public QueueNodeHearbeat(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<QueueNodeHearbeat>();
-        }
-
-        [Function("QueueNodeHearbeat")]
-        public void Run([QueueTrigger("myqueue-items", Connection = "AzureWebJobsStorage")] string msg)
-        {
-            var hb = JsonSerializer.Deserialize<NodeHeartbeatEntry>(msg);
-            
+    [Function("QueueNodeHearbeat")]
+    public void Run([QueueTrigger("myqueue-items", Connection = "AzureWebJobsStorage")] string msg)
+    {
+        var hb = JsonSerializer.Deserialize<NodeHeartbeatEntry>(msg);
+        
 
 
-            _logger.LogInformation($"heartbeat: {msg}");
-        }
+        _logger.LogInformation($"heartbeat: {msg}");
     }
 }
 
---
+++ src/ApiService/Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
