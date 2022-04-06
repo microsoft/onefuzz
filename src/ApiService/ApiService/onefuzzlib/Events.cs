8566bd9a35a7e50a595bfa7248fa54f0a8fa89ab
Cheick Keita <chkeita@microsoft.com>
2022-04-06 13:47:22 -0700

    [WIP ] continue port of  QueueNodeHearbeat

---
+++ src/ApiService/ApiService/onefuzzlib/Events.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.OneFuzz.Service.OneFuzzLib
+{
+    internal class Events
+    {
+        public void SendEvent(BaseEvent anEvent) {
+            var eventType = anEvent.GetEventType();
+
+            var event_message = new EventMessage(
+                Guid.NewGuid(),
+                eventType,
+                anEvent,
+                Guid.NewGuid(), // todo
+                "test" //todo
+            );
+            
+        }
+    }
+}
