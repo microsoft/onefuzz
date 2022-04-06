ae51fe5160d7344e204e6404e4f7b06f95d01753
Noah Harper <noharper@microsoft.com>
2022-04-06 13:36:25 -0700

    Setting to serializer.

--- src/ApiService/ApiService/QueueProxyUpdate.cs
+++ src/ApiService/ApiService/QueueProxyUpdate.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
+using Microsoft.OneFuzz.Service.OneFuzzLib.Orm;
 
 namespace Microsoft.OneFuzz.Service;
 
@@ -17,7 +18,7 @@ public class QueueProxyUpdate
     [Function("QueueProxyUpdate")]
     public void Run([QueueTrigger("myqueue-items", Connection = "AzureWebJobsStorage")] string msg)
     {
-        var hb = JsonSerializer.Deserialize<ProxyHeartbeat>(msg);
+        var hb = JsonSerializer.Deserialize<ProxyHeartbeat>(msg, EntityConverter.GetJsonSerializerOptions()).EnsureNotNull($"wrong data {msg}");;
         
         _logger.LogInformation($"heartbeat: {msg}");
     }
