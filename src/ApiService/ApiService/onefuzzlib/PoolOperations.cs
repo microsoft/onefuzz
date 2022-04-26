b07a8dd7229d4ac8abc4b294509cf1905f00bfd4
Cheick Keita <chkeita@microsoft.com>
2022-04-26 11:35:51 -0700

    breaking the circular dependency between JobOperations and TaskOperations

--- src/ApiService/ApiService/onefuzzlib/PoolOperations.cs
+++ src/ApiService/ApiService/onefuzzlib/PoolOperations.cs
@@ -9,12 +9,10 @@ public interface IPoolOperations {
 
 public class PoolOperations : StatefulOrm<Pool, PoolState>, IPoolOperations {
     private IConfigOperations _configOperations;
-    private ITaskOperations _taskOperations;
 
-    public PoolOperations(IStorage storage, ILogTracer log, IServiceConfig config, IConfigOperations configOperations, ITaskOperations taskOperations)
+    public PoolOperations(IStorage storage, ILogTracer log, IServiceConfig config, IConfigOperations configOperations)
         : base(storage, log, config) {
         _configOperations = configOperations;
-        _taskOperations = taskOperations;
     }
 
     public async Async.Task<Result<Pool, Error>> GetByName(string poolName) {
