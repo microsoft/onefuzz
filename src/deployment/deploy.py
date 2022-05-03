90d6b27f55d2ed1104f2a19e525e85052ac9d336
Cheick Keita <chkeita@microsoft.com>
2022-05-03 14:29:59 -0700

    [testing] hard code deployment of -net function

--- src/deployment/deploy.py
+++ src/deployment/deploy.py
@@ -1289,12 +1289,12 @@ def main() -> None:
         )
         states = rbac_only_states
     else:
-        if args.dotnet_deploy:
-            logger.info("deploying dotnet and python services for Azure functions")
-            after_python = full_deployment_states.index(("api", Client.deploy_app)) + 1
-            full_deployment_states.insert(
-                after_python, ("dotnet-api", Client.deploy_dotnet_app)
-            )
+        # if args.dotnet_deploy:
+        logger.info("deploying dotnet and python services for Azure functions")
+        after_python = full_deployment_states.index(("api", Client.deploy_app)) + 1
+        full_deployment_states.insert(
+            after_python, ("dotnet-api", Client.deploy_dotnet_app)
+        )
         states = full_deployment_states
 
     if args.start_at != states[0][0]:
