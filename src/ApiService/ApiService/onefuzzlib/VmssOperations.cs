c600d20ad35c2c0f594390a10a638dacd4475789
Teo Voinea <58236992+tevoinea@users.noreply.github.com>
2022-05-03 14:45:28 +0000

    Missed a file

--- src/ApiService/ApiService/onefuzzlib/VmssOperations.cs
+++ src/ApiService/ApiService/onefuzzlib/VmssOperations.cs
@@ -6,14 +6,10 @@ using Microsoft.Rest.Azure;
 namespace Microsoft.OneFuzz.Service;
 
 public interface IVmssOperations {
-<<<<<<< HEAD
-    public Async.Task<OneFuzzResultVoid> UpdateExtensions(Guid name, IList<VirtualMachineScaleSetExtensionData> extensions);
     Async.Task<OneFuzzResultVoid> UpdateScaleInProtection(Guid name, Guid vmId, bool protectFromScaleIn);
     Async.Task<OneFuzzResult<string>> GetInstanceId(Guid name, Guid vmId);
-=======
     Async.Task<OneFuzzResultVoid> UpdateExtensions(Guid name, IList<VirtualMachineScaleSetExtensionData> extensions);
     Async.Task<VirtualMachineScaleSetData> GetVmss(Guid name);
->>>>>>> 8c73f84b87fa5ddb59d45e398f5e8475aaf43543
 
     Async.Task<bool> DeleteVmss(Guid name, bool? forceDeletion = null);
 
@@ -89,77 +85,6 @@ public class VmssOperations : IVmssOperations {
         }
     }
 
-<<<<<<< HEAD
-    public async Async.Task<OneFuzzResult<string>> GetInstanceId(Guid name, Guid vmId) {
-        _log.Info($"get instance ID for scaleset node: {name}:{vmId}");
-        var vmIdStr = vmId.ToString();
-        var vmss = (await _creds.GetResourceGroupResource().GetVirtualMachineScaleSetAsync(name.ToString())).Value;
-        var instance = await vmss.GetVirtualMachineScaleSetVms()
-            .SelectAwait(async instance => await instance.GetAsync())
-            .Where(instance => string.Equals(instance.Value.Data.VmId, vmIdStr))
-            .FirstOrDefaultAsync();
-
-        return instance switch {
-            Response<VirtualMachineScaleSetVmResource> i =>
-                OneFuzzResult<string>.Ok(i.Value.Data.VmId),
-            _ =>
-                OneFuzzResult<string>.Error(
-                    ErrorCode.UNABLE_TO_FIND,
-                    $"unable to find scaleset machine: {name}:{vmId}"
-                )
-        };
-    }
-
-    public async Async.Task<OneFuzzResultVoid> UpdateScaleInProtection(Guid name, Guid vmId, bool protectFromScaleIn) {
-        var instanceIdResult = await GetInstanceId(name, vmId);
-
-        if (!instanceIdResult.IsOk) {
-            return OneFuzzResultVoid.Error(instanceIdResult.ErrorV);
-        }
-        var instanceId = instanceIdResult.OkV;
-
-        var vmss = (await _creds.GetResourceGroupResource().GetVirtualMachineScaleSetAsync(name.ToString())).Value;
-        var instanceVmResult = await vmss.GetVirtualMachineScaleSetVmAsync(instanceId);
-
-        if (instanceVmResult == null) {
-            return OneFuzzResultVoid.Error(
-                ErrorCode.UNABLE_TO_FIND,
-                $"unable to find vm instance: {name}:{instanceId}"
-            );
-        }
-        var instanceVm = instanceVmResult.Value;
-
-        var newProtectionPolicy = instanceVm.Data.ProtectionPolicy ?? new VirtualMachineScaleSetVmProtectionPolicy();
-        newProtectionPolicy.ProtectFromScaleIn = protectFromScaleIn;
-
-        instanceVm.Data.ProtectionPolicy = newProtectionPolicy;
-
-        var update = await vmss.GetVirtualMachineScaleSetVms().CreateOrUpdateAsync(WaitUntil.Started, instanceId, instanceVm.Data);
-        var unableToUpdate = OneFuzzResultVoid.Error(
-                ErrorCode.UNABLE_TO_UPDATE,
-                $"unable to set protection policy on {vmId}:{instanceId}"
-        );
-        if (update == null) {
-            return unableToUpdate;
-        }
-
-        var rawResponse = update.GetRawResponse();
-        if (rawResponse.IsError) {
-            var errorContent = rawResponse.Content.ToString();
-            const string instanceNotFound = " is not an active Virtual Machine Scale Set VM instanceId.";
-            if (errorContent.Contains(instanceNotFound)
-            && instanceVm.Data.ProtectionPolicy.ProtectFromScaleIn == false
-            && instanceVm.Data.ProtectionPolicy.ProtectFromScaleIn == protectFromScaleIn) {
-                _log.Info($"Tried to remove scale in protection on node {instanceId} but the instance no longer exists");
-                return OneFuzzResultVoid.Ok();
-            }
-            return unableToUpdate;
-        }
-
-        _log.Info($"Successfully set scale in protection on node {vmId} to {protectFromScaleIn}");
-        return OneFuzzResultVoid.Ok();
-    }
-=======
     public async Async.Task<IDictionary<Guid, string>> ListInstanceIds(Guid name) {
         _log.Verbose($"get instance IDs for scaleset {name}");
         var results = new Dictionary<Guid, string>();
@@ -246,5 +171,4 @@ public class VmssOperations : IVmssOperations {
     }
 
 
->>>>>>> 8c73f84b87fa5ddb59d45e398f5e8475aaf43543
 }
