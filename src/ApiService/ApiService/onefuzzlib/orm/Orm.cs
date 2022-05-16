a3e63df18d5c351d821671021f549f21abb7cdaf
Cheick Keita <chkeita@microsoft.com>
2022-05-16 09:25:02 -0700

    Upgrading dotnet version to 6.0.300 (#1947)

--- src/ApiService/ApiService/onefuzzlib/orm/Orm.cs
+++ src/ApiService/ApiService/onefuzzlib/orm/Orm.cs
@@ -149,7 +149,7 @@ namespace ApiService.OneFuzzLib.Orm {
                 });
 
             if (func != null) {
-                _logTracer.Info($"processing state update: {typeof(T)} - PartitionKey {_partitionKeyGetter?.Value() } {_rowKeyGetter?.Value() } - %s");
+                _logTracer.Info($"processing state update: {typeof(T)} - PartitionKey {_partitionKeyGetter?.Value()} {_rowKeyGetter?.Value()} - %s");
                 return await func(entity);
             }
             return null;
