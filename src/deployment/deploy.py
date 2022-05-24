0b494895a4be3a16335fb7e3f801412b6fe66a98
AdamLeclerc-Microsoft <103067949+AdamLeclerc-Microsoft@users.noreply.github.com>
2022-05-24 13:19:29 -0700

    fix type hint

--- src/deployment/deploy.py
+++ src/deployment/deploy.py
@@ -153,7 +153,7 @@ class Client:
         subscription_id: Optional[str],
         admins: List[UUID],
         allowed_aad_tenants: List[UUID],
-        enable_dotnet: List[str]
+        enable_dotnet: str
     ):
         self.subscription_id = subscription_id
         self.resource_group = resource_group
