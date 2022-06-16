94e6b8a1da9f9d013208a157fc8de9d95e4c8550
George Pollard <gpollard@microsoft.com>
2022-06-16 01:46:34 +0000

    More tests for WorkerRunning events

--- src/ApiService/Tests/Functions/AgentEventsTests.cs
+++ src/ApiService/Tests/Functions/AgentEventsTests.cs
@@ -35,7 +35,7 @@ public abstract class AgentEventsTestsBase : FunctionTestBase {
 
     [Fact]
     public async Async.Task WorkerEventMustHaveDoneOrRunningSet() {
-        var func = new AgentEvents(Logger, CreateTestContext());
+        var func = new AgentEvents(Logger, Context);
 
         var data = new NodeStateEnvelope(
             MachineId: Guid.NewGuid(),
@@ -48,15 +48,13 @@ public abstract class AgentEventsTestsBase : FunctionTestBase {
 
     [Fact]
     public async Async.Task WorkerDone_WithSuccessfulResult_ForRunningTask_MarksTaskAsStopping() {
-        var ctx = CreateTestContext();
-
-        await ctx.InsertAll(
+        await Context.InsertAll(
                 new Node(poolName, machineId, poolId, poolVersion),
                 // task state is running
                 new Task(jobId, taskId, TaskState.Running, Os.Linux,
                     new TaskConfig(jobId, null, new TaskDetails(TaskType.Coverage, 100))));
 
-        var func = new AgentEvents(Logger, ctx);
+        var func = new AgentEvents(Logger, Context);
 
         var data = new NodeStateEnvelope(
             MachineId: machineId,
@@ -69,7 +67,7 @@ public abstract class AgentEventsTestsBase : FunctionTestBase {
         var result = await func.Run(TestHttpRequestData.FromJson("POST", data));
         Assert.Equal(HttpStatusCode.OK, result.StatusCode);
 
-        var task = await ctx.TaskOperations.SearchAll().SingleAsync();
+        var task = await Context.TaskOperations.SearchAll().SingleAsync();
 
         // should have transitioned into stopping
         Assert.Equal(TaskState.Stopping, task.State);
@@ -77,16 +75,14 @@ public abstract class AgentEventsTestsBase : FunctionTestBase {
 
     [Fact]
     public async Async.Task WorkerDone_WithFailedResult_ForRunningTask_MarksTaskAsStoppingAndErrored() {
-        var ctx = CreateTestContext();
-
-        await ctx.InsertAll(
+        await Context.InsertAll(
                 new Node(poolName, machineId, poolId, poolVersion),
                 // task state is running
                 new Task(jobId, taskId, TaskState.Running, Os.Linux,
                     new TaskConfig(jobId, null, new TaskDetails(TaskType.Coverage, 100))));
 
 
-        var func = new AgentEvents(Logger, ctx);
+        var func = new AgentEvents(Logger, Context);
 
         var data = new NodeStateEnvelope(
             MachineId: machineId,
@@ -99,22 +95,20 @@ public abstract class AgentEventsTestsBase : FunctionTestBase {
         var result = await func.Run(TestHttpRequestData.FromJson("POST", data));
         Assert.Equal(HttpStatusCode.OK, result.StatusCode);
 
-        var task = await ctx.TaskOperations.SearchAll().SingleAsync();
+        var task = await Context.TaskOperations.SearchAll().SingleAsync();
         Assert.Equal(TaskState.Stopping, task.State); // should have transitioned into stopping
         Assert.Equal(ErrorCode.TASK_FAILED, task.Error?.Code); // should be an error
     }
 
     [Fact]
     public async Async.Task WorkerDone_ForNonStartedTask_MarksTaskAsFailed() {
-        var ctx = CreateTestContext();
-
-        await ctx.InsertAll(
+        await Context.InsertAll(
             new Node(poolName, machineId, poolId, poolVersion),
             // task state is scheduled, not running
             new Task(jobId, taskId, TaskState.Scheduled, Os.Linux,
                 new TaskConfig(jobId, null, new TaskDetails(TaskType.Coverage, 100))));
 
-        var func = new AgentEvents(Logger, ctx);
+        var func = new AgentEvents(Logger, Context);
 
         var data = new NodeStateEnvelope(
             MachineId: machineId,
@@ -127,10 +121,84 @@ public abstract class AgentEventsTestsBase : FunctionTestBase {
         var result = await func.Run(TestHttpRequestData.FromJson("POST", data));
         Assert.Equal(HttpStatusCode.OK, result.StatusCode);
 
-        var task = await ctx.TaskOperations.SearchAll().SingleAsync();
+        var task = await Context.TaskOperations.SearchAll().SingleAsync();
 
         // should be failed - it never started running
         Assert.Equal(TaskState.Stopping, task.State);
         Assert.Equal(ErrorCode.TASK_FAILED, task.Error?.Code);
+    }
+
+    [Fact]
+    public async Async.Task WorkerRunning_ForMissingTask_ReturnsError() {
+        await Context.InsertAll(
+            new Node(poolName, machineId, poolId, poolVersion));
+
+        var func = new AgentEvents(Logger, Context);
+        var data = new NodeStateEnvelope(
+            MachineId: machineId,
+            Event: new WorkerEvent(Running: new WorkerRunningEvent(taskId)));
+
+        var result = await func.Run(TestHttpRequestData.FromJson("POST", data));
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+        Assert.Contains("unable to find task", BodyAsString(result));
+    }
+
+    [Fact]
+    public async Async.Task WorkerRunning_ForMissingNode_ReturnsError() {
+        await Context.InsertAll(
+            new Task(jobId, taskId, TaskState.Running, Os.Linux,
+                new TaskConfig(jobId, null, new TaskDetails(TaskType.Coverage, 0))));
+
+        var func = new AgentEvents(Logger, Context);
+        var data = new NodeStateEnvelope(
+            MachineId: machineId,
+            Event: new WorkerEvent(Running: new WorkerRunningEvent(taskId)));
+
+        var result = await func.Run(TestHttpRequestData.FromJson("POST", data));
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+        Assert.Contains("unable to find node", BodyAsString(result));
+    }
+
+    [Fact]
+    public async Async.Task WorkerRunning_HappyPath() {
+        await Context.InsertAll(
+            new Node(poolName, machineId, poolId, poolVersion),
+            new Task(jobId, taskId, TaskState.Running, Os.Linux,
+                new TaskConfig(jobId, null, new TaskDetails(TaskType.Coverage, 0))));
+
+        var func = new AgentEvents(Logger, Context);
+        var data = new NodeStateEnvelope(
+            MachineId: machineId,
+            Event: new WorkerEvent(Running: new WorkerRunningEvent(taskId)));
+
+        var result = await func.Run(TestHttpRequestData.FromJson("POST", data));
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+        // perform checks in parallel
+        await Async.Task.WhenAll(
+            Async.Task.Run(async () => {
+                // task should be marked running
+                var task = await Context.TaskOperations.SearchAll().SingleAsync();
+                Assert.Equal(TaskState.Running, task.State);
+            }),
+            Async.Task.Run(async () => {
+                // node should now be marked busy
+                var node = await Context.NodeOperations.SearchAll().SingleAsync();
+                Assert.Equal(NodeState.Busy, node.State);
+            }),
+            Async.Task.Run(async () => {
+                // there should be a node-task with correct values
+                var nodeTask = await Context.NodeTasksOperations.SearchAll().SingleAsync();
+                Assert.Equal(machineId, nodeTask.MachineId);
+                Assert.Equal(taskId, nodeTask.TaskId);
+                Assert.Equal(NodeTaskState.Running, nodeTask.State);
+            }),
+            Async.Task.Run(async () => {
+                // there should be a task-event with correct values
+                var taskEvent = await Context.TaskEventOperations.SearchAll().SingleAsync();
+                Assert.Equal(taskId, taskEvent.TaskId);
+                Assert.Equal(machineId, taskEvent.MachineId);
+                Assert.Equal(new WorkerEvent(Running: new WorkerRunningEvent(taskId)), taskEvent.EventData);
+            }));
     }
 }
