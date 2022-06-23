c4bc9055fe3d51f49e7d0cad8f472d1b5dbd9128
stas <statis@microsoft.com>
2022-06-23 12:35:14 -0700

    proposed fix from here:
    https://github.com/Azure/azure-functions-durable-python/issues/194#issuecomment-710670377

---
+++ src/api-service/__app__/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+import certifi.core
+
+
+def override_where() -> str:
+    """overrides certifi.core.where to return actual location of cacert.pem"""
+    # change this to match the location of cacert.pem
+    return os.path.abspath(
+        "cacert.pem"
+    )  # or to whatever location you know contains the copy of cacert.pem
+
+
+os.environ["REQUESTS_CA_BUNDLE"] = override_where()
+certifi.core.where = override_where
