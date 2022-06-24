61ca7be6bc25549f3d0884325ea9268296f7dded
Cheick Keita <kcheick@gmail.com>
2022-06-24 08:53:45 -0700

    Update src/deployment/deploy.py

--- src/deployment/deploy.py
+++ src/deployment/deploy.py
@@ -152,7 +152,7 @@ class Client:
         subscription_id: Optional[str],
         admins: List[UUID],
         allowed_aad_tenants: List[UUID],
-        enable_dotnet: List[str] = ["timer_tasks"],
+        enable_dotnet: List[str],
     ):
         self.subscription_id = subscription_id
         self.resource_group = resource_group
