92d70f16a6a70aaa03dc63c037ec5c834749eeee
Stas <stishkin@live.com>
2022-06-27 14:43:55 -0700

    Update src/api-service/__app__/__init__.py
    Co-authored-by: George Pollard <porges@porg.es>

--- src/api-service/__app__/__init__.py
+++ src/api-service/__app__/__init__.py
@@ -5,6 +5,8 @@ import certifi.core
 
 def override_where() -> str:
     """overrides certifi.core.where to return actual location of cacert.pem"""
+    # see:
+    # https://github.com/Azure/azure-functions-durable-python/issues/194#issuecomment-710670377
     # change this to match the location of cacert.pem
     return os.path.abspath(
         "cacert.pem"
