3c2918a1d2e8b1e3cdfdba3a1edc4d039e0f343c
Teo Voinea <tevoinea@microsoft.com>
2022-06-27 17:41:21 +0000

    .

--- src/agent/onefuzz-task/src/local/cmd.rs
+++ src/agent/onefuzz-task/src/local/cmd.rs
@@ -1,13 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+use crate::local::{
+    common::add_common_config, generic_analysis, generic_crash_report, generic_generator,
+    libfuzzer, libfuzzer_crash_report, libfuzzer_fuzz, libfuzzer_merge, libfuzzer_regression,
+    libfuzzer_test_input, radamsa, test_input, tui::TerminalUi,
+};
 #[cfg(any(target_os = "linux", target_os = "windows"))]
 use crate::local::{coverage, dotnet_coverage};
-use crate::local::{
-    common::add_common_config, generic_analysis, generic_crash_report,
-    generic_generator, libfuzzer, libfuzzer_crash_report, libfuzzer_fuzz, libfuzzer_merge,
-    libfuzzer_regression, libfuzzer_test_input, radamsa, test_input, tui::TerminalUi,
-};
 use anyhow::{Context, Result};
 use clap::{App, Arg, SubCommand};
 use crossterm::tty::IsTty;
