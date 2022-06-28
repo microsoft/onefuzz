3c3aeaa5691e301eae13bdb8564660c88ff08b4b
George Pollard <gpollard@microsoft.com>
2022-06-29 08:19:44 +1200

    Add test for StatefulOrm static constructor (#2092)
    `StatefulOrm` performs checks in the static constructor. Make sure that all these checks pass.

---
+++ src/ApiService/Tests/StatefulOrmTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using ApiService.OneFuzzLib.Orm;
+using Xunit;
+
+namespace Tests;
+
+public class StatefulOrmTests {
+    public static IEnumerable<object[]> GetStatefulOrmTypes() {
+        var statefulOrmType = typeof(StatefulOrm<,,>);
+        foreach (var type in statefulOrmType.Assembly.GetTypes()) {
+            var baseType = type.BaseType;
+            if (baseType is not null
+                && baseType.IsGenericType
+                && baseType.GetGenericTypeDefinition() == statefulOrmType) {
+                yield return new object[] { type };
+            }
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(GetStatefulOrmTypes))]
+    public void EnsureValid(Type statefulOrmType) {
+        // make sure that the classes are all valid,
+        // there is a static constructor on StatefulOrm that performs checks,
+        // so explicitly run the class constructor for the base type:
+        RuntimeHelpers.RunClassConstructor(statefulOrmType.BaseType!.TypeHandle);
+    }
+}
