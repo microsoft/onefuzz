9f10b864878cf55099d6729d78b9dcd76f5a7fb3
George Pollard <gpollard@microsoft.com>
2022-07-01 00:05:57 +0000

    Finish AgentRegistration implementation

--- src/ApiService/ApiService/AgentRegistration.cs
+++ src/ApiService/ApiService/AgentRegistration.cs
@@ -1,4 +1,4 @@
-using System.Web;
+﻿using System.Web;
 using Azure.Storage.Sas;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -32,8 +32,8 @@ public class AgentRegistration {
 
     private async Async.Task<HttpResponseData> Get(HttpRequestData req) {
         var uri = HttpUtility.ParseQueryString(req.Url.Query);
-        var machineId = uri["machine_id"];
-        if (machineId is null || !Guid.TryParse(machineId, out var machineGuid)) {
+        var rawMachineId = uri["machine_id"];
+        if (rawMachineId is null || !Guid.TryParse(rawMachineId, out var machineId)) {
             return await _context.RequestHandling.NotOk(
                 req,
                 new Error(
@@ -42,15 +42,15 @@ public class AgentRegistration {
                 "agent registration");
         }
 
-        var agentNode = await _context.NodeOperations.GetByMachineId(machineGuid);
+        var agentNode = await _context.NodeOperations.GetByMachineId(machineId);
         if (agentNode is null) {
             return await _context.RequestHandling.NotOk(
                 req,
                 new Error(
                     ErrorCode.INVALID_REQUEST,
-                    new string[] { $"unable to find a registration associated with machine_id '{machineGuid}'" }),
+                    new string[] { $"unable to find a registration associated with machine_id '{machineId}'" }),
                 "agent registration");
-        } 
+        }
 
         var pool = await _context.PoolOperations.GetByName(agentNode.PoolName);
         if (!pool.IsOk) {
@@ -62,10 +62,10 @@ public class AgentRegistration {
                 "agent registration");
         }
 
-        return await RequestHandling.Ok(req, await CreateRegistrationResponse(machineGuid, pool.OkV));
+        return await RequestHandling.Ok(req, await CreateRegistrationResponse(pool.OkV));
     }
 
-    private async Async.Task<AgentRegistrationResponse> CreateRegistrationResponse(Guid machineId, Pool pool) {
+    private async Async.Task<AgentRegistrationResponse> CreateRegistrationResponse(Pool pool) {
         var baseAddress = _context.Creds.GetInstanceUrl();
         var eventsUrl = new Uri(baseAddress, "/api/agents/events");
         var commandsUrl = new Uri(baseAddress, "/api/agents/commands");
@@ -82,7 +82,60 @@ public class AgentRegistration {
             WorkQueue: workQueue);
     }
 
-    private Async.Task<HttpResponseData> Post(HttpRequestData req) {
-        throw new NotImplementedException();
+    private async Async.Task<HttpResponseData> Post(HttpRequestData req) {
+        var uri = HttpUtility.ParseQueryString(req.Url.Query);
+        var rawMachineId = uri["machine_id"];
+        if (rawMachineId is null || !Guid.TryParse(rawMachineId, out var machineId)) {
+            return await _context.RequestHandling.NotOk(
+                req,
+                new Error(
+                    ErrorCode.INVALID_REQUEST,
+                    new string[] { "'machine_id' query parameter must be provided" }),
+                "agent registration");
+        }
+
+        var rawPoolName = uri["pool_name"];
+        if (rawPoolName is null || !PoolName.TryParse(rawPoolName, out var poolName)) {
+            return await _context.RequestHandling.NotOk(
+                req,
+                new Error(
+                    ErrorCode.INVALID_REQUEST,
+                    new string[] { "'pool_name' query parameter must be provided" }),
+                "agent registration");
+        }
+
+        var rawScalesetId = uri["scaleset_id"];
+        var scalesetId = rawScalesetId is null ? (Guid?)null : Guid.Parse(rawScalesetId);
+
+        var version = uri["version"] ?? "1.0.0";
+
+        _log.Info($"registration request: machine_id: {machineId} pool_name: {poolName} scaleset_id: {scalesetId} version: {version}");
+        var poolResult = await _context.PoolOperations.GetByName(poolName);
+        if (!poolResult.IsOk) {
+            return await _context.RequestHandling.NotOk(
+                req,
+                new Error(
+                    Code: ErrorCode.INVALID_REQUEST,
+                    Errors: new[] { $"unable to find pool '{poolName}'" }),
+                "agent registration");
+        }
+
+        var pool = poolResult.OkV;
+
+        var existingNode = await _context.NodeOperations.GetByMachineId(machineId);
+        if (existingNode is not null) {
+            await _context.NodeOperations.Delete(existingNode);
+        }
+
+        var node = new Node(
+            PoolName: poolName,
+            PoolId: pool.PoolId,
+            MachineId: machineId,
+            ScalesetId: scalesetId,
+            Version: version);
+
+        await _context.NodeOperations.Replace(node);
+
+        return await RequestHandling.Ok(req, await CreateRegistrationResponse(pool));
     }
 }
