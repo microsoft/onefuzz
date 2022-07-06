c4081cca5a40f3f902253dbc7586bb278618ae48
Joe Ranweiler <joranwei@microsoft.com>
2022-07-06 17:57:09 +0000

    Put all artifacts in own dirs

--- src/ci/dotnet-fuzzing-tools.ps1
+++ src/ci/dotnet-fuzzing-tools.ps1
@@ -8,10 +8,13 @@ $LIBFUZZER_DOTNET_REPO = 'https://github.com/Metalnem/libfuzzer-dotnet'
 $LIBFUZZER_DOTNET_COMMIT = '55d84f84b3540c864371e855c2a5ecb728865d97'
 
 # Script below assumes an absolute path.
-$INSTALL_DIR = "${env:GITHUB_WORKSPACE}/artifacts/third-party/dotnet-fuzzing-windows"
+$ARTIFACTS = "${env:GITHUB_WORKSPACE}/artifacts/third-party/dotnet-fuzzing-windows"
 
-# Create install destination.
-mkdir $INSTALL_DIR
+# Create install destinations.
+mkdir $ARTIFACTS
+mkdir $ARTIFACTS/libfuzzer-dotnet
+mkdir $ARTIFACTS/LibFuzzerDotnetLoader
+mkdir $ARTIFACTS/sharpfuzz
 
 # Prepare SharpFuzz dependency for sibling project reference, build instrumentor.
 #
@@ -20,17 +23,13 @@ pushd src/agent
 git clone $SHARPFUZZ_REPO
 pushd sharpfuzz
 git checkout $SHARPFUZZ_COMMIT
-mkdir $INSTALL_DIR/sharpfuzz
-dotnet publish src/SharpFuzz.CommandLine -f net6.0 -c Release -o $INSTALL_DIR/sharpfuzz --sc -r win10-x64
+dotnet publish src/SharpFuzz.CommandLine -f net6.0 -c Release -o $ARTIFACTS/sharpfuzz --sc -r win10-x64
 popd
 popd
 
 # Build SharpFuzz and our dynamic loader harness for `libfuzzer-dotnet`.
 pushd src/agent/LibFuzzerDotnetLoader
-mkdir out
-dotnet publish . -c Release -o out --sc -r win10-x64 -p:PublishSingleFile=true
-cp out/LibFuzzerDotnetLoader.exe $INSTALL_DIR
-cp out/LibFuzzerDotnetLoader.pdb $INSTALL_DIR
+dotnet publish . -c Release -o $ARTIFACTS/LibFuzzerDotnetLoader --sc -r win10-x64 -p:PublishSingleFile=true
 popd
 
 # Build `libfuzzer-dotnet`.
@@ -38,6 +37,6 @@ git clone $LIBFUZZER_DOTNET_REPO
 pushd libfuzzer-dotnet
 git checkout $LIBFUZZER_DOTNET_COMMIT
 clang -g -O2 -fsanitize=fuzzer libfuzzer-dotnet-windows.cc -o libfuzzer-dotnet.exe
-cp libfuzzer-dotnet.exe $INSTALL_DIR
-cp libfuzzer-dotnet.pdb $INSTALL_DIR
+cp libfuzzer-dotnet.exe $ARTIFACTS/libfuzzer-dotnet
+cp libfuzzer-dotnet.pdb $ARTIFACTS/libfuzzer-dotnet
 popd
