75ef698a183efd9751b1535777a430f3950bf445
Cheick Keita <chkeita@microsoft.com>
2022-08-02 11:10:41 -0700

    extracting nework interface query

--- src/ApiService/ApiService/onefuzzlib/IpOperations.cs
+++ src/ApiService/ApiService/onefuzzlib/IpOperations.cs
@@ -20,14 +20,17 @@ public interface IIpOperations {
     public Async.Task<string?> GetScalesetInstanceIp(Guid scalesetId, Guid machineId);
 }
 
+
 public class IpOperations : IIpOperations {
     private ILogTracer _logTracer;
 
     private IOnefuzzContext _context;
+    private readonly NetworkInterfaceQuery _networkInterfaceQuery;
 
     public IpOperations(ILogTracer log, IOnefuzzContext context) {
         _logTracer = log;
         _context = context;
+        _networkInterfaceQuery = new NetworkInterfaceQuery(context);
     }
 
     public async Async.Task<NetworkInterfaceResource> GetPublicNic(string resourceGroup, string name) {
@@ -50,36 +53,7 @@ public class IpOperations : IIpOperations {
         await _context.Creds.GetResourceGroupResource().GetPublicIPAddressAsync(name).Result.Value.DeleteAsync(WaitUntil.Started);
     }
 
-    record IpConfigurationsProperties(string privateIPAddress);
-
-    record IpConfigurations(IpConfigurationsProperties properties);
-
-    record NetworkInterfaceProperties(List<IpConfigurations> ipConfigurations);
-
-    record NetworkInterface(NetworkInterfaceProperties properties);
-
-    record ValueList<T>(List<T> value);
-
-    private async Task<List<string>> ListIps(Guid scalesetId, string instanceId) {
-
-        var token = _context.Creds.GetIdentity().GetToken(
-            new TokenRequestContext(
-                new[] { $"https://management.azure.com" }));
-
-        using HttpClient client = new HttpClient();
-        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.Token);
-        var baseUrl = new Uri($"https://management.azure.com/");
-        var requestURl = baseUrl + $"subscriptions/{_context.Creds.GetSubscription()}/resourceGroups/{_context.Creds.GetBaseResourceGroup()}/providers/Microsoft.Compute/virtualMachineScaleSets/{scalesetId}/virtualMachines/{instanceId}/networkInterfaces?api-version=2021-08-01";
-        var response = await client.GetAsync(requestURl);
-        if (response.IsSuccessStatusCode) {
-            var responseStream = await response.Content.ReadAsStreamAsync();
-            var nics = await JsonSerializer.DeserializeAsync<ValueList<NetworkInterface>>(responseStream);
-            if (nics != null)
-                return nics.value.SelectMany(x => x.properties.ipConfigurations.Select(i => i.properties.privateIPAddress)).WhereNotNull().ToList();
-        }
-
-        return new List<string>();
-    }
+    
 
     public async Task<string?> GetScalesetInstanceIp(Guid scalesetId, Guid machineId) {
         var instance = await _context.VmssOperations.GetInstanceId(scalesetId, machineId);
@@ -87,7 +61,56 @@ public class IpOperations : IIpOperations {
             return null;
         }
 
-        var ips = await ListIps(scalesetId, instance.OkV);
+        var ips = await _networkInterfaceQuery.ListInstancePrivateIps(scalesetId, instance.OkV);
         return ips.FirstOrDefault();
     }
+    
+    
+    /// <summary>
+    /// Query the Scaleset network interface using the rest api directly because
+    /// the api does not seems to support this :
+    /// https://github.com/Azure/azure-sdk-for-net/issues/30253#issuecomment-1202447362
+    /// </summary>
+    class NetworkInterfaceQuery
+    {
+        record IpConfigurationsProperties(string privateIPAddress);
+
+        record IpConfigurations(IpConfigurationsProperties properties);
+
+        record NetworkInterfaceProperties(List<IpConfigurations> ipConfigurations);
+
+        record NetworkInterface(NetworkInterfaceProperties properties);
+
+        record ValueList<T>(List<T> value);
+
+        private readonly IOnefuzzContext _context;
+
+        public NetworkInterfaceQuery(IOnefuzzContext context) {
+            _context = context;
+        }
+
+        
+        public async Task<List<string>> ListInstancePrivateIps(Guid scalesetId, string instanceId) {
+
+            var token = _context.Creds.GetIdentity().GetToken(
+                new TokenRequestContext(
+                    new[] { $"https://management.azure.com" }));
+
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.Token);
+            var baseUrl = new Uri($"https://management.azure.com/");
+            // https://docs.microsoft.com/en-us/rest/api/virtualnetwork/network-interface-in-vm-ss/get-virtual-machine-scale-set-network-interface?tabs=HTTP
+            var requestURl = baseUrl + $"subscriptions/{_context.Creds.GetSubscription()}/resourceGroups/{_context.Creds.GetBaseResourceGroup()}/providers/Microsoft.Compute/virtualMachineScaleSets/{scalesetId}/virtualMachines/{instanceId}/networkInterfaces?api-version=2021-08-01";
+            var response = await client.GetAsync(requestURl);
+            if (response.IsSuccessStatusCode) {
+                var responseStream = await response.Content.ReadAsStreamAsync();
+                var nics = await JsonSerializer.DeserializeAsync<ValueList<NetworkInterface>>(responseStream);
+                if (nics != null)
+                    return nics.value.SelectMany(x => x.properties.ipConfigurations.Select(i => i.properties.privateIPAddress)).WhereNotNull().ToList();
+            }
+            return new List<string>();
+        }
+    }
 }
+
+
