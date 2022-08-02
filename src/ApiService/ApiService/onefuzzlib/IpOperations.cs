342c8de5e639d1271081772255819495d07998de
Cheick Keita <chkeita@microsoft.com>
2022-08-02 10:58:19 -0700

    - added private ip address query - updated Azure.ResourceManager.Storage to non beta release

--- src/ApiService/ApiService/onefuzzlib/IpOperations.cs
+++ src/ApiService/ApiService/onefuzzlib/IpOperations.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Azure;
+using Azure.Core;
 using Azure.ResourceManager.Network;
+using Faithlife.Utility;
 
 namespace Microsoft.OneFuzz.Service;
 
@@ -46,19 +50,44 @@ public class IpOperations : IIpOperations {
         await _context.Creds.GetResourceGroupResource().GetPublicIPAddressAsync(name).Result.Value.DeleteAsync(WaitUntil.Started);
     }
 
+    record IpConfigurationsProperties(string privateIPAddress);
+
+    record IpConfigurations(IpConfigurationsProperties properties);
+
+    record NetworkInterfaceProperties(List<IpConfigurations> ipConfigurations);
+
+    record NetworkInterface(NetworkInterfaceProperties properties);
+
+    record ValueList<T>(List<T> value);
+
+    private async Task<List<string>> ListIps(Guid scalesetId, string instanceId) {
+
+        var token = _context.Creds.GetIdentity().GetToken(
+            new TokenRequestContext(
+                new[] { $"https://management.azure.com" }));
+
+        using HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.Token);
+        var baseUrl = new Uri($"https://management.azure.com/");
+        var requestURl = baseUrl + $"subscriptions/{_context.Creds.GetSubscription()}/resourceGroups/{_context.Creds.GetBaseResourceGroup()}/providers/Microsoft.Compute/virtualMachineScaleSets/{scalesetId}/virtualMachines/{instanceId}/networkInterfaces?api-version=2021-08-01";
+        var response = await client.GetAsync(requestURl);
+        if (response.IsSuccessStatusCode) {
+            var responseStream = await response.Content.ReadAsStreamAsync();
+            var nics = await JsonSerializer.DeserializeAsync<ValueList<NetworkInterface>>(responseStream);
+            if (nics != null)
+                return nics.value.SelectMany(x => x.properties.ipConfigurations.Select(i => i.properties.privateIPAddress)).WhereNotNull().ToList();
+        }
+
+        return new List<string>();
+    }
+
     public async Task<string?> GetScalesetInstanceIp(Guid scalesetId, Guid machineId) {
         var instance = await _context.VmssOperations.GetInstanceId(scalesetId, machineId);
         if (!instance.IsOk) {
             return null;
         }
 
-        // var resourceGroup = _context.Creds.GetBaseResourceGroup();
-        // var id = VirtualMachineScaleSetResource.CreateResourceIdentifier(_context.Creds.GetSubscription(), resourceGroup, scalesetId.ToString());
-        // var vmss =  _context.Creds.ArmClient.GetVirtualMachineScaleSetResource(id);
-        // var vm = await vmss.GetVirtualMachineScaleSetVmAsync(machineId.ToString());
-
-        // todo: find how to get the private ip
-
-        return "";
+        var ips = await ListIps(scalesetId, instance.OkV);
+        return ips.FirstOrDefault();
     }
 }
