b75c411617f258cc5a52a590c3d941887c82495d
Cheick Keita <chkeita@microsoft.com>
2022-08-04 11:45:37 -0700

    fix name

--- src/ApiService/ApiService/Functions/ReproVmss.cs
+++ src/ApiService/ApiService/Functions/ReproVmss.cs
@@ -15,7 +15,7 @@ public class ReproVmss {
         _context = context;
     }
 
-    [Function("Node")]
+    [Function("repro_vmss")]
     public Async.Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "GET", "PATCH", "POST", "DELETE")] HttpRequestData req) {
         return _auth.CallIfUser(req, r => r.Method switch {
             "GET" => Get(r),
