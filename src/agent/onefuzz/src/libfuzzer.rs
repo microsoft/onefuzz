106bcc810e1cb85bebdf95b7b53c175f8d49971b
Joe Ranweiler <joranwei@microsoft.com>
2022-08-04 19:16:22 +0000

    Format

--- src/agent/onefuzz/src/libfuzzer.rs
+++ src/agent/onefuzz/src/libfuzzer.rs
@@ -430,7 +430,12 @@ mod tests {
         let input_file = temp_setup_dir.path().join("input.txt");
         write_file(&input_file, "input").await?;
 
-        let fuzzer = LibFuzzer::new(bad_bin, options.clone(), env.clone(), &temp_setup_dir.path());
+        let fuzzer = LibFuzzer::new(
+            bad_bin,
+            options.clone(),
+            env.clone(),
+            &temp_setup_dir.path(),
+        );
 
         // verify catching bad exits with -help=1
         assert!(
@@ -453,7 +458,12 @@ mod tests {
             "checking false without inputs"
         );
 
-        let fuzzer = LibFuzzer::new(good_bin, options.clone(), env.clone(), &temp_setup_dir.path());
+        let fuzzer = LibFuzzer::new(
+            good_bin,
+            options.clone(),
+            env.clone(),
+            &temp_setup_dir.path(),
+        );
         // verify good exits with -help=1
         assert!(
             fuzzer.verify(true, None).await.is_ok(),
