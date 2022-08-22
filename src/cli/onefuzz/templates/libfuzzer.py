0baaa5e47f8976a562035d2e0acb4f672f406176
Joe Ranweiler <joranwei@microsoft.com>
2022-08-22 20:40:30 +0000

    Format

--- src/cli/onefuzz/templates/libfuzzer.py
+++ src/cli/onefuzz/templates/libfuzzer.py
@@ -29,8 +29,12 @@ LIBFUZZER_MAGIC_STRING = b"ERROR: libFuzzer"
 #
 # The `dotnet_coverage` and `dotnet_crash_report` tasks must invoke the underlying DLL, so
 # it must not be statically linked into the platform-native executable wrapper.
-LIBFUZZER_DOTNET_LOADER_PATH_LINUX = "{tools_dir}/dotnet-fuzzing-linux/LibFuzzerDotnetLoader/LibFuzzerDotnetLoader.dll"
-LIBFUZZER_DOTNET_LOADER_PATH_WINDOWS = "{tools_dir}/dotnet-fuzzing-windows/LibFuzzerDotnetLoader/LibFuzzerDotnetLoader.dll"
+LIBFUZZER_DOTNET_LOADER_PATH_LINUX = (
+    "{tools_dir}/dotnet-fuzzing-linux/LibFuzzerDotnetLoader/LibFuzzerDotnetLoader.dll"
+)
+LIBFUZZER_DOTNET_LOADER_PATH_WINDOWS = (
+    "{tools_dir}/dotnet-fuzzing-windows/LibFuzzerDotnetLoader/LibFuzzerDotnetLoader.dll"
+)
 
 
 class QemuArch(Enum):
