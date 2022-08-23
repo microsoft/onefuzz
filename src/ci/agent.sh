3a4739366ff668eb69776720b0853581ef4c7cfb
George Pollard <gpollard@microsoft.com>
2022-08-23 02:13:13 +0000

    Fix time/chrono audit failures

--- src/ci/agent.sh
+++ src/ci/agent.sh
@@ -51,11 +51,9 @@ cargo fmt -- --check
 # RUSTSEC-2019-0036: a dependency `failure` (pulled from proc-maps) has type confusion vulnerability
 # RUSTSEC-2021-0065: a dependency `anymap` is no longer maintained
 # RUSTSEC-2020-0077: `memmap` dependency unmaintained, via `symbolic` (see: `getsentry/symbolic#304`)
-# RUSTSEC-2020-0159: potential segfault in `time`, not yet patched (#1366)
-# RUSTSEC-2020-0071: potential segfault in `chrono`, not yet patched (#1366)
 # RUSTSEC-2022-0048: xml-rs is unmaintained
 # RUSTSEC-2021-0139: ansi_term is unmaintained
-cargo audit --deny warnings --deny unmaintained --deny unsound --deny yanked --ignore RUSTSEC-2020-0016 --ignore RUSTSEC-2020-0036 --ignore RUSTSEC-2019-0036 --ignore RUSTSEC-2021-0065 --ignore RUSTSEC-2020-0159 --ignore RUSTSEC-2020-0071 --ignore RUSTSEC-2020-0077 --ignore RUSTSEC-2022-0048 --ignore RUSTSEC-2021-0139
+cargo audit --deny warnings --deny unmaintained --deny unsound --deny yanked --ignore RUSTSEC-2020-0016 --ignore RUSTSEC-2020-0036 --ignore RUSTSEC-2019-0036 --ignore RUSTSEC-2021-0065 --ignore RUSTSEC-2020-0077 --ignore RUSTSEC-2022-0048 --ignore RUSTSEC-2021-0139
 cargo-license -j > data/licenses.json
 cargo build --release --locked
 cargo clippy --release --locked --all-targets -- -D warnings
