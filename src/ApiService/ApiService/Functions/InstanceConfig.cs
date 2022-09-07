4e9f57814484c672ac480c3639e4b09c693ce53e
Noah Harper <noharper@microsoft.com>
2022-09-07 09:42:13 -0700

    Formatting.

--- src/ApiService/ApiService/Functions/InstanceConfig.cs
+++ src/ApiService/ApiService/Functions/InstanceConfig.cs
@@ -17,7 +17,7 @@ public class InstanceConfig {
 
     [Function("InstanceConfig")]
     public Async.Task<HttpResponseData> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "GET", "POST", Route="api/instance_config")] HttpRequestData req) {
+        [HttpTrigger(AuthorizationLevel.Anonymous, "GET", "POST", Route = "api/instance_config")] HttpRequestData req) {
         return _auth.CallIfUser(req, r => r.Method switch {
             "GET" => Get(r),
             "POST" => Post(r),
