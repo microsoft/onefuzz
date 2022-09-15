3f86fc8689c5b1785ce9048bc6fded629dbb68d0
Stas <stishkin@live.com>
2022-09-15 15:45:39 -0700

    fix some bugs (#2406)
    * - fix queries in timer retention
    
    - do not discard proxy record after proxy state is processed, since that record needs to persist
    
    * addressing comments
    
    Co-authored-by: stas <statis@microsoft.com>

--- src/ApiService/ApiService/onefuzzlib/VmOperations.cs
+++ src/ApiService/ApiService/onefuzzlib/VmOperations.cs
@@ -218,8 +218,10 @@ public class VmOperations : IVmOperations {
                 extensionName,
                 extension
             );
-        } catch (RequestFailedException ex) when (ex.Status == 409 && ex.Message.Contains("VM is marked for deletion")) {
-            _logTracer.Info(ex.Message);
+        } catch (RequestFailedException ex) when
+            (ex.Status == 409 &&
+                (ex.Message.Contains("VM is marked for deletion") || ex.Message.Contains("The request failed due to conflict with a concurrent request."))) {
+            _logTracer.Info($"Tried to create extension but failed due to {ex.Message}");
         }
         return;
     }
