5f04b7627bafbcc1324d8358d532dca4a3af27a2
Noah McGregor Harper <74685766+nharper285@users.noreply.github.com>
2022-09-21 23:54:33 +0000

    Removing remaining refs to images outside of models.

--- src/cli/onefuzz/templates/__init__.py
+++ src/cli/onefuzz/templates/__init__.py
@@ -16,8 +16,6 @@ from onefuzztypes.primitives import Container, Directory, File
 from ..job_templates.job_monitor import JobMonitor
 
 ELF_MAGIC = b"\x7fELF"
-DEFAULT_LINUX_IMAGE = "Canonical:UbuntuServer:18.04-LTS:latest"
-DEFAULT_WINDOWS_IMAGE = "MicrosoftWindowsDesktop:Windows-10:win10-21h2-pro:latest"
 
 
 class StoppedEarly(Exception):
