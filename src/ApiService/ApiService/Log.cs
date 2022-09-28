83ff9428cdb055956519a7ea829f6730c2b06535
stas <statis@microsoft.com>
2022-09-28 14:16:39 -0700

    fix null ref exception

--- src/ApiService/ApiService/Log.cs
+++ src/ApiService/ApiService/Log.cs
@@ -25,7 +25,9 @@ public struct LogStringHandler {
     }
 
     public void AppendFormatted<T>(T message) {
-        _builder.Append(message?.ToString());
+        if (message is not null) {
+            _builder.Append(message.ToString());
+        }
     }
 
     public void AppendFormatted<T>(T message, string? format) {
@@ -43,7 +45,9 @@ public struct LogStringHandler {
         }
     }
 
-    public override string ToString() => _builder.ToString();
+    private bool HasData => _builder is not null && _builder.Length > 0;
+
+    public override string ToString() => this.HasData ? _builder.ToString() : String.Empty;
     public IReadOnlyDictionary<string, string>? Tags => _tags;
 }
 
@@ -105,14 +109,12 @@ class AppInsights : ILog {
             }
         }
 
-
         Dictionary<string, double>? copyMetrics = null;
         if (metrics is not null) {
             copyMetrics = new(metrics);
         }
         _telemetryClient.TrackException(ex, copyTags, copyMetrics);
-
-        Log(correlationId, $"{message} : {ex.Message}", SeverityLevel.Error, tags, caller);
+        Log(correlationId, $"[{message}] {ex.Message}", SeverityLevel.Error, tags, caller);
     }
 
     public void Flush() {
