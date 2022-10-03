1d5bad4679227b38ccd848e08a1cb15865ebdd37
Noah McGregor Harper <74685766+nharper285@users.noreply.github.com>
2022-10-03 20:25:50 +0000

    Convert Error to Warning on Poll State Transition Failure.

--- src/agent/onefuzz-task/src/tasks/generic/input_poller.rs
+++ src/agent/onefuzz-task/src/tasks/generic/input_poller.rs
@@ -251,7 +251,7 @@ impl<M> InputPoller<M> {
         if let Err(err) = &result {
             // We must maintain a valid state, and we can logically recover from
             // any failed action or invalid transition.
-            error!(
+            warn!(
                 "State Transition Failed. Resetting state to 'Ready.' Error: {}",
                 err
             );
