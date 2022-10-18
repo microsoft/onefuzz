4d6a4bec468516c036058e13a4752ca5757f7776
Joe Ranweiler <joranwei@microsoft.com>
2022-10-18 12:07:32 -0700

    Add support for `dotnet_dll` integration tests

--- src/integration-tests/integration-test.py
+++ src/integration-tests/integration-test.py
@@ -52,6 +52,7 @@ class TaskTestState(Enum):
 class TemplateType(Enum):
     libfuzzer = "libfuzzer"
     libfuzzer_dotnet = "libfuzzer_dotnet"
+    libfuzzer_dotnet_dll = "libfuzzer_dotnet_dll"
     libfuzzer_qemu_user = "libfuzzer_qemu_user"
     afl = "afl"
     radamsa = "radamsa"
@@ -76,6 +77,9 @@ class Integration(BaseModel):
     test_repro: Optional[bool] = Field(default=True)
     target_options: Optional[List[str]]
     inject_fake_regression: bool = Field(default=False)
+    target_class: Optional[str]
+    target_method: Optional[str]
+    setup_dir: Optional[str]
 
 
 TARGETS: Dict[str, Integration] = {
@@ -315,7 +319,11 @@ class TestOnefuzz:
 
             self.logger.info("launching: %s", target)
 
-            setup = Directory(os.path.join(path, target)) if config.use_setup else None
+            if config.setup_dir is None:
+                setup = Directory(os.path.join(path, target)) if config.use_setup else None
+            else:
+                setup = config.setup_dir
+
             target_exe = File(os.path.join(path, target, config.target_exe))
             inputs = (
                 Directory(os.path.join(path, target, config.inputs))
@@ -355,6 +363,28 @@ class TestOnefuzz:
                     duration=duration,
                     vm_count=1,
                     target_options=config.target_options,
+                )
+            elif config.template == TemplateType.libfuzzer_dotnet_dll:
+                if setup is None:
+                    raise Exception("setup required for libfuzzer_dotnet_dll")
+                if config.target_class is None:
+                    raise Exception("target_class required for libfuzzer_dotnet_dll")
+                if config.target_method is None:
+                    raise Exception("target_method required for libfuzzer_dotnet_dll")
+
+                job = self.of.template.libfuzzer.dotnet_dll(
+                    self.project,
+                    target,
+                    BUILD,
+                    pools[config.os].name,
+                    target_dll=config.target_exe,
+                    inputs=inputs,
+                    setup_dir=setup,
+                    duration=duration,
+                    vm_count=1,
+                    target_options=config.target_options,
+                    target_class=config.target_class,
+                    target_method=config.target_method,
                 )
             elif config.template == TemplateType.libfuzzer_qemu_user:
                 job = self.of.template.libfuzzer.qemu_user(
