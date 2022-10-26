a975c308d61b128217e19ed4f5bdf38b1dbe3147
Teo Voinea <Teodor.Voinea@microsoft.com>
2022-10-26 14:16:44 +0000

    PR comments

--- src/ApiService/ApiService/Functions/QueueFileChanges.cs
+++ src/ApiService/ApiService/Functions/QueueFileChanges.cs
@@ -31,23 +31,26 @@ public class QueueFileChanges {
 
     [Function("QueueFileChanges")]
     public async Async.Task Run(
-        [QueueTrigger(QueueFileChangesQueueName, Connection = "AzureWebJobsStorage")] string msg,
-        int dequeueCount) {
+        [QueueTrigger(QueueFileChangesQueueName, Connection = "AzureWebJobsStorage")] string msg) {
         var fileChangeEvent = JsonSerializer.Deserialize<JsonDocument>(msg, EntityConverter.GetJsonSerializerOptions());
-        var lastTry = dequeueCount == MAX_DEQUEUE_COUNT;
 
         _ = fileChangeEvent ?? throw new ArgumentException("Unable to parse queue trigger as JSON");
 
         // check type first before calling Azure APIs
         const string eventType = "eventType";
+        const string expectedEventType = "Microsoft.Storage.BlobCreated";
         if (!fileChangeEvent.RootElement.TryGetProperty(eventType, out var eventTypeElement)
-            || eventTypeElement.GetString() != "Microsoft.Storage.BlobCreated") {
+            || eventTypeElement.GetString() != expectedEventType) {
+            _log.WithTag("queueMessage", msg)
+                .Info($"Expected fileChangeEvent to contain a property named '{eventType}' with value equal to {expectedEventType}");
             return;
         }
 
         const string topic = "topic";
         if (!fileChangeEvent.RootElement.TryGetProperty(topic, out var topicElement)
             || !_storage.CorpusAccounts().Contains(new ResourceIdentifier(topicElement.GetString()!))) {
+            _log.WithTag("queueMessage", msg)
+                .Info($"Expected fileChangeEvent to contain a property named '{topic}' with a value contained in corpus accounts");
             return;
         }
 
@@ -57,10 +60,10 @@ public class QueueFileChanges {
             // the message for us. The difference is for the poison queue, we're handling the
             // requeuing ourselves because azure functions doesn't support retry policies
             // for queue based functions.
-            await FileAdded(_log, fileChangeEvent, false);
+
+            await FileAdded(_log, fileChangeEvent, isLastRetryAttempt: false);
         } catch {
-            var queueLocation = lastTry ? QueueFileChangesPoisonQueueName : QueueFileChangesQueueName;
-            await RequeueMessage(msg, queueLocation);
+            await RequeueMessage(msg);
         }
     }
 
@@ -76,7 +79,7 @@ public class QueueFileChanges {
         await _notificationOperations.NewFiles(Container.Parse(container), path, isLastRetryAttempt);
     }
 
-    private async Async.Task RequeueMessage(string msg, string queueName) {
+    private async Async.Task RequeueMessage(string msg) {
         var json = JsonNode.Parse(msg);
 
         // Messages that are 'manually' requeued by us as opposed to being requeued by the azure functions runtime
@@ -84,14 +87,16 @@ public class QueueFileChanges {
         // That's why we need to keep track of it ourselves.
         var newCustomDequeueCount = 0;
         if (json!["data"]?["customDequeueCount"] != null) {
-            newCustomDequeueCount = json["data"]!["customDequeueCount"]!.GetValue<int>() + 1;
+            newCustomDequeueCount = json["data"]!["customDequeueCount"]!.GetValue<int>();
         }
 
-        json!["data"]!["customDequeueCount"] = newCustomDequeueCount;
-
-        if (newCustomDequeueCount > 6) {
-            _log.Warning($"Message retried for over a week with no success: {msg}");
+        var queueName = QueueFileChangesQueueName;
+        if (newCustomDequeueCount > MAX_DEQUEUE_COUNT) {
+            _log.Warning($"Message retried more than {MAX_DEQUEUE_COUNT} times with no success: {msg}");
+            queueName = QueueFileChangesPoisonQueueName;
         }
+
+        json!["data"]!["customDequeueCount"] = newCustomDequeueCount + 1;
 
         await _context.Queue.QueueObject(
             queueName,
@@ -106,9 +111,8 @@ public class QueueFileChanges {
     // 2 - 25 mins
     // 3 - 2 hours 5 mins
     // 4 - 10 hours 25 mins
-    // 5 - 2 days 4 hours 5 mins
-    // >6 - 7 days +/- 1 day of random variance so all the messages don't dequeue at the same time
-    private static TimeSpan CalculateExponentialBackoff(int dequeueCount) {
+    // >5 - 2 days +/ 6 hours variance so all the messages don't dequeue at the same time
+    public static TimeSpan CalculateExponentialBackoff(int dequeueCount) {
         var numMinutes = Math.Pow(5, dequeueCount);
         var backoff = TimeSpan.FromMinutes(numMinutes);
         var maxBackoff = MaxTtl();
@@ -116,8 +120,8 @@ public class QueueFileChanges {
     }
 
     private static TimeSpan MaxTtl() {
-        var variance = new Random().Next(0, 48);
-        return TimeSpan.FromHours((24 * 6) + variance);
+        var variance = Random.Shared.Next(0, 6);
+        return TimeSpan.FromHours((24 * 2) + variance);
     }
 
 }
