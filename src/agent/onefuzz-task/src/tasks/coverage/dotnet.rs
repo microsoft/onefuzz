6c981f613d6f430c93718499ab010adc75b674fe
Cheick Keita <chkeita@microsoft.com>
2022-11-23 16:32:19 -0800

    Making machine identity a parameter of the agent config (#2649)
    * making machine identity a parameter
    
    * build fix
    
    * build fix
    
    * format

--- src/agent/onefuzz-task/src/tasks/coverage/dotnet.rs
+++ src/agent/onefuzz-task/src/tasks/coverage/dotnet.rs
@@ -266,7 +266,7 @@ impl<'a> TaskContext<'a> {
         // Try to expand `target_exe` with support for `{tools_dir}`.
         //
         // Allows using `LibFuzzerDotnetLoader.exe` from a shared tools container.
-        let expand = Expand::new().tools_dir(tools_dir);
+        let expand = Expand::new(&self.config.common.machine_identity).tools_dir(tools_dir);
         let expanded = expand.evaluate_value(&self.config.target_exe.to_string_lossy())?;
         let expanded_path = Path::new(&expanded);
 
@@ -293,7 +293,7 @@ impl<'a> TaskContext<'a> {
     async fn command_for_input(&self, input: &Path) -> Result<Command> {
         let target_exe = self.target_exe().await?;
 
-        let expand = Expand::new()
+        let expand = Expand::new(&self.config.common.machine_identity)
             .machine_id()
             .await?
             .input_path(input)
