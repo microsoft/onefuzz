b26d14dbdd55243b0e0b8a6c2b8a80379a19d119
Joe Ranweiler <joranwei@microsoft.com>
2022-12-21 12:37:31 -0800

    Add deserialization compat functions

--- src/agent/onefuzz-file-format/src/coverage/binary.rs
+++ src/agent/onefuzz-file-format/src/coverage/binary.rs
@@ -8,13 +8,27 @@ pub mod v0;
 pub mod v1;
 
 #[derive(Serialize, Deserialize)]
-#[serde(tag = "version")]
+#[serde(tag = "version", content = "coverage")]
 pub enum BinaryCoverageJson {
-    #[serde(rename = "0")]
+    #[serde(rename = "0.1")]
     V0(v0::BinaryCoverageJson),
 
-    #[serde(rename = "1")]
+    #[serde(rename = "1.0")]
     V1(v1::BinaryCoverageJson),
+}
+
+impl BinaryCoverageJson {
+    pub fn deserialize(text: &str) -> Result<Self> {
+        // Try unversioned legacy format.
+        let v0 = serde_json::from_str::<v0::BinaryCoverageJson>(text);
+
+        if let Ok(v0) = v0 {
+            return Ok(Self::V0(v0));
+        }
+
+        // Try versioned formats.
+        Ok(serde_json::from_str(text)?)
+    }
 }
 
 impl TryFrom<BinaryCoverageJson> for BinaryCoverage {
