cbc1801cb246c1a0e10450b08c3e28bc77b42d77
Cheick Keita <chkeita@microsoft.com>
2023-01-23 12:14:16 -0800

    fix build

--- src/ci/unset-versions.sh
+++ src/ci/unset-versions.sh
@@ -8,8 +8,8 @@ set -ex
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 cd ${SCRIPT_DIR}/../../
 
-SET_VERSIONS="src/pytypes/onefuzztypes/__version__.py src/api-service/__app__/onefuzzlib/__version__.py src/cli/onefuzz/__version__.py"
-SET_REQS="src/cli/requirements.txt src/api-service/__app__/requirements.txt"
+SET_VERSIONS="src/pytypes/onefuzztypes/__version__.py src/cli/onefuzz/__version__.py"
+SET_REQS="src/cli/requirements.txt"
 
 sed -i 's/__version__ = .*/__version__ = "0.0.0"/' ${SET_VERSIONS}
 sed -i "s/onefuzztypes==.*/onefuzztypes==0.0.0/" ${SET_REQS}
