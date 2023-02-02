add16a1fbd1406debbd9d48d7f1b525fa7dc727b
George Pollard <gpollard@microsoft.com>
2023-02-02 01:20:27 +0000

    Add Windows support

--- src/ci/check-dependencies.sh
+++ src/ci/check-dependencies.sh
@@ -19,6 +19,9 @@ function get-deps {
 
 function check {
     wanted=$2
+    if [ "$(uname)" != 'Linux' ]; then 
+        wanted=$3
+    fi
     got=$(get-deps "$1")
     if ! difference=$(diff -u --color <(echo "$wanted") <(echo "$got")); then 
         echo "unexpected dependencies for $1"
@@ -43,7 +46,25 @@ libpthread.so.0
 libunwind-ptrace.so.0
 libunwind-x86_64.so.8
 libunwind.so.8
-linux-vdso.so.1"
+linux-vdso.so.1" \
+\
+"ADVAPI32.dll
+KERNEL32.DLL
+KERNELBASE.dll
+MSASN1.dll
+PSAPI.DLL
+RPCRT4.dll
+SSPICLI.DLL
+apphelp.dll
+bcrypt.dll
+crypt32.dll
+dbghelp.dll
+msvcrt.dll
+ntdll.dll
+sechost.dll
+secur32.dll
+ucrtbase.dll
+ws2_32.dll"
 
 check "$script_dir/../agent/target/release/onefuzz-agent" \
 "/lib64/ld-linux-x86-64.so.2
@@ -53,11 +74,33 @@ liblzma.so.5
 libm.so.6
 libpthread.so.0
 libunwind.so.8
-linux-vdso.so.1"
+linux-vdso.so.1" \
+\
+"KERNEL32.DLL
+KERNELBASE.dll
+MSASN1.dll
+RPCRT4.dll
+SSPICLI.DLL
+advapi32.dll
+apphelp.dll
+bcrypt.dll
+crypt32.dll
+msvcrt.dll
+ntdll.dll
+sechost.dll
+secur32.dll
+ucrtbase.dll
+ws2_32.dll"
 
 check "$script_dir/../agent/target/release/srcview" \
 "/lib64/ld-linux-x86-64.so.2
 libc.so.6
 libgcc_s.so.1
 libpthread.so.0
-linux-vdso.so.1"
+linux-vdso.so.1" \
+\
+"KERNEL32.DLL
+KERNELBASE.dll
+apphelp.dll
+bcrypt.dll
+ntdll.dll"
