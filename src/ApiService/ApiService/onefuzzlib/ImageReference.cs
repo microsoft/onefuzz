f006f791d322cb4bf6b3508b8f2994e9242850a8
George Pollard <gpollard@microsoft.com>
2023-03-22 02:33:22 +0000

    Fix Focal image name

--- src/ApiService/ApiService/onefuzzlib/ImageReference.cs
+++ src/ApiService/ApiService/onefuzzlib/ImageReference.cs
@@ -13,7 +13,7 @@ namespace Microsoft.OneFuzz.Service;
 
 public static class DefaultImages {
     public static readonly ImageReference Windows = ImageReference.MustParse("MicrosoftWindowsDesktop:Windows-10:win10-21h2-pro:latest");
-    public static readonly ImageReference Linux = ImageReference.MustParse("Canonical:0000-com-ubuntu-server-focal:20_04-lts:latest");
+    public static readonly ImageReference Linux = ImageReference.MustParse("Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest");
 }
 
 [JsonConverter(typeof(Converter<ImageReference>))]
