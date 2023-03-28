3c097c1376ae359ffb260835c99c240845e66536
Cheick Keita <chkeita@microsoft.com>
2023-03-28 10:18:14 -0700

    specify a container instead of a directory

--- src/cli/onefuzz/templates/libfuzzer.py
+++ src/cli/onefuzz/templates/libfuzzer.py
@@ -357,7 +357,7 @@ class Libfuzzer(Command):
         analyzer_env: Optional[Dict[str, str]] = None,
         tools: Optional[Container] = None,
         extra_container: Optional[Container] = None,
-        crashes: Optional[Directory] = None,
+        crashes: Optional[Container] = None,
     ) -> Optional[Job]:
         """
         Basic libfuzzer job
@@ -372,6 +372,9 @@ class Libfuzzer(Command):
 
         if readonly_inputs:
             self.onefuzz.containers.get(readonly_inputs)
+
+        if crashes:
+            self.onefuzz.containers.get(crashes)
 
         if dryrun:
             return None
@@ -412,6 +415,9 @@ class Libfuzzer(Command):
 
         if readonly_inputs:
             helper.containers[ContainerType.readonly_inputs] = readonly_inputs
+
+        if crashes:
+            helper.containers[ContainerType.crashes] = crashes
 
         if analyzer_exe is not None:
             helper.define_containers(ContainerType.analysis)
@@ -643,7 +649,7 @@ class Libfuzzer(Command):
         expect_crash_on_failure: bool = False,
         notification_config: Optional[NotificationConfig] = None,
         extra_container: Optional[Container] = None,
-        crashes: Optional[Directory] = None,
+        crashes: Optional[Container] = None,
     ) -> Optional[Job]:
         pool = self.onefuzz.pools.get(pool_name)
 
@@ -653,6 +659,9 @@ class Libfuzzer(Command):
 
         if readonly_inputs:
             self.onefuzz.containers.get(readonly_inputs)
+
+        if crashes:
+            self.onefuzz.containers.get(crashes)
 
         # We _must_ proactively specify the OS based on pool.
         #
@@ -706,6 +715,9 @@ class Libfuzzer(Command):
 
         if readonly_inputs:
             helper.containers[ContainerType.readonly_inputs] = readonly_inputs
+
+        if crashes:
+            helper.containers[ContainerType.crashes] = crashes
 
         # Assumes that `libfuzzer-dotnet` and supporting tools were uploaded upon deployment.
         fuzzer_tools_container = Container(
@@ -871,7 +883,7 @@ class Libfuzzer(Command):
         check_retry_count: Optional[int] = 300,
         check_fuzzer_help: bool = True,
         extra_container: Optional[Container] = None,
-        crashes: Optional[Directory] = None,
+        crashes: Optional[Container] = None,
     ) -> Optional[Job]:
         """
         libfuzzer tasks, wrapped via qemu-user (PREVIEW FEATURE)
@@ -923,6 +935,10 @@ class Libfuzzer(Command):
             helper.containers[ContainerType.inputs] = existing_inputs
         else:
             helper.define_containers(ContainerType.inputs)
+
+        if crashes:
+            self.onefuzz.containers.get(crashes)
+            helper.containers[ContainerType.crashes] = crashes
 
         fuzzer_containers = [
             (ContainerType.setup, helper.containers[ContainerType.setup]),
