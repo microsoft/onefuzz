5519ad03963d30061fc7b47992874d677cb3e7c8
Teo Voinea <58236992+tevoinea@users.noreply.github.com>
2023-04-05 13:14:48 -0400

    Update feature configuration package and use different ids for feature flags (#2980)

--- src/ApiService/ApiService/FeatureFlags.cs
+++ src/ApiService/ApiService/FeatureFlags.cs
@@ -1,7 +1,7 @@
 ﻿namespace Microsoft.OneFuzz.Service;
 
 public static class FeatureFlagConstants {
-    public const string EnableScribanOnly = "EnableScribanOnly";
+    public const string RenderOnlyScribanTemplates = "RenderOnlyScribanTemplates";
     public const string EnableNodeDecommissionStrategy = "EnableNodeDecommissionStrategy";
-    public const string EnableValidateNotificationConfigSemantics = "EnableValidateNotificationConfigSemantics";
+    public const string SemanticNotificationConfigValidation = "SemanticNotificationConfigValidation";
 }
