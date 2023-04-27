dd4899a29871b3d48db57caaacc7c32befdbbbe2
Noah McGregor Harper <74685766+nharper285@users.noreply.github.com>
2023-04-27 21:41:25 +0000

    Fixing test metrics.

--- src/ApiService/IntegrationTests/Fakes/TestMetrics.cs
+++ src/ApiService/IntegrationTests/Fakes/TestMetrics.cs
@@ -9,15 +9,11 @@ public sealed class TestMetrics : IMetrics {
 
     public List<BaseMetric> Metrics { get; } = new();
     public List<MetricMessage> CustomMetrics { get; } = new();
+    public TestMetrics(ILogTracer log, IOnefuzzContext context)
+        : base(log, context) { }
     public void LogMetric(BaseMetric metric) {
         Metrics.Add(metric);
     }
-
-    public Async.Task QueueCustomMetric(MetricMessage message) {
-        CustomMetrics.Add(message);
-        return Async.Task.CompletedTask;
-    }
-
     public Async.Task SendMetric(int metricValue, BaseMetric customDimensions) {
         Metrics.Add(customDimensions);
         return Async.Task.CompletedTask;
