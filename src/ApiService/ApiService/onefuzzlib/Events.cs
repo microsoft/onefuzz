68deb5ff7aec8f62020617d5c34a5376d53b50dd
Teo Voinea <tevoinea@microsoft.com>
2023-05-01 14:00:15 +0000

    Add another truncation case

--- src/ApiService/ApiService/onefuzzlib/Events.cs
+++ src/ApiService/ApiService/onefuzzlib/Events.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using Azure;
 using Microsoft.OneFuzz.Service.OneFuzzLib.Orm;
 
 namespace Microsoft.OneFuzz.Service {
@@ -41,8 +42,27 @@ namespace Microsoft.OneFuzz.Service {
         }
 
         public async Async.Task QueueSignalrEvent(EventMessage message) {
-            var ev = new SignalREvent("events", new List<EventMessage>() { message });
-            await _queue.SendMessage("signalr-events", JsonSerializer.Serialize(ev, _options), StorageType.Config);
+            var tags = new (string, string)[] {
+                ("event_type", message.EventType.ToString()),
+                ("event_id", message.EventId.ToString())
+            };
+            try {
+                var ev = new SignalREvent("events", new List<EventMessage>() { message });
+                await _queue.SendMessage("signalr-events", JsonSerializer.Serialize(ev, _options), StorageType.Config);
+            } catch (RequestFailedException ex) {
+                if (ex.Message.Contains("The request body is too large") && message.Event is ITruncatable<BaseEvent> truncatableEvent) {
+                    _log.WithTags(tags).Warning($"The EventMessage was too large for Azure Queue. Truncating event data and trying again.");
+                    message = message with {
+                        Event = truncatableEvent.Truncate(1000)
+                    };
+                    var ev = new SignalREvent("events", new List<EventMessage>() { message });
+                    await _queue.SendMessage("signalr-events", JsonSerializer.Serialize(ev, _options), StorageType.Config);
+                } else {
+                    // Not handled
+                    throw ex;
+                }
+            }
+
         }
 
         public async Async.Task SendEvent(BaseEvent anEvent) {
