436c2988145059a3c2657ec96584f264465e7351
Teo Voinea <58236992+tevoinea@users.noreply.github.com>
2023-06-22 15:35:58 +0000

    fmt

--- src/ApiService/ApiService/onefuzzlib/RententionPolicy.cs
+++ src/ApiService/ApiService/onefuzzlib/RententionPolicy.cs
@@ -1,24 +1,20 @@
 ﻿namespace Microsoft.OneFuzz.Service;
 
 
-public interface IRetentionPolicy
-{
+public interface IRetentionPolicy {
     DateOnly GetExpiryDate();
 }
 
-public class RetentionPolicyUtils
-{
+public class RetentionPolicyUtils {
     public const string EXPIRY_TAG = "Expiry";
     public static KeyValuePair<string, string> CreateExpiryDateTag(DateOnly expiryDate) =>
         new(EXPIRY_TAG, expiryDate.ToString());
 
-    public static DateOnly? GetExpiryDateTagFromTags(IDictionary<string, string>? blobTags)
-    {
+    public static DateOnly? GetExpiryDateTagFromTags(IDictionary<string, string>? blobTags) {
         if (blobTags != null &&
             blobTags.TryGetValue(EXPIRY_TAG, out var expiryTag) &&
             !string.IsNullOrWhiteSpace(expiryTag) &&
-            DateOnly.TryParse(expiryTag, out var expiryDate))
-        {
+            DateOnly.TryParse(expiryTag, out var expiryDate)) {
             return expiryDate;
         }
         return null;
