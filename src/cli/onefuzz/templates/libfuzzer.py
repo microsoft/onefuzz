7ef2746b729dc5f00dd98a8a029057a6d56ea465
George Pollard <gpollard@microsoft.com>
2023-07-23 21:01:24 +0000

    Attempt .NET fix

--- src/cli/onefuzz/templates/libfuzzer.py
+++ src/cli/onefuzz/templates/libfuzzer.py
@@ -21,7 +21,7 @@ LIBFUZZER_MAGIC_STRING = b"ERROR: libFuzzer"
 # The loader DLL is managed, but links platform-specific code. Task VMs must pull the
 # tools container that matches their platform (which will contain the correct DLL).
 LIBFUZZER_DOTNET_LOADER_PATH = (
-    "{tools_dir}/LibFuzzerDotnetLoader/LibFuzzerDotnetLoader.dll"
+    "{tools_dir}/LibFuzzerDotnetLoader/LibFuzzerDotnetLoader"
 )
 
 
