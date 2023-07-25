1a48247c06bff61b1634954a3bbce37134c9c7fd
Noah McGregor Harper <74685766+nharper285@users.noreply.github.com>
2023-07-25 21:39:09 +0000

    Updating context.

--- src/ApiService/ApiService/onefuzzlib/JobResultOperations.cs
+++ src/ApiService/ApiService/onefuzzlib/JobResultOperations.cs
@@ -8,82 +8,83 @@ public interface IJobResultOperations : IOrm<JobResult> {
     JobResult UpdateResult(JobResult result, HeartbeatType type);
     Async.Task<OneFuzzResult<JobResult>> CreateOrUpdate(Guid jobId, HeartbeatType resultType);
 
-    public class JobResultOperations : Orm<JobResult>, IJobResultOperations {
+}
+public class JobResultOperations : Orm<JobResult>, IJobResultOperations {
 
-        public JobResultOperations(ILogger<JobResultOperations> log, IOnefuzzContext context)
-            : base(log, context) {
+    public JobResultOperations(ILogger<JobResultOperations> log, IOnefuzzContext context)
+        : base(log, context) {
+    }
+
+    public async Async.Task<JobResult?> GetJobResult(Guid jobId) {
+        return await SearchByPartitionKeys(new[] { jobId.ToString() }).SingleOrDefaultAsync();
+    }
+
+    public JobResult UpdateResult(JobResult result, HeartbeatType type) {
+        // return await SearchByPartitionKeys(new[] { jobId.ToString() }).SingleOrDefaultAsync();
+
+        var newResult = result;
+        int newValue;
+        switch (type) {
+            case HeartbeatType.NewCrashingInput:
+                newValue = result.NewCrashingInput + 1;
+                newResult = result with { NewCrashingInput = newValue };
+                break;
+            case HeartbeatType.NewReport:
+                newValue = result.NewReport + 1;
+                newResult = result with { NewReport = newValue };
+                break;
+            case HeartbeatType.NewUniqueReport:
+                newValue = result.NewReport + 1;
+                newResult = result with { NewReport = newValue };
+                break;
+            case HeartbeatType.NewRegressionReport:
+                newValue = result.NewRegressionReport + 1;
+                newResult = result with { NewRegressionReport = newValue };
+                break;
+            default:
+                _logTracer.LogInformation("Invalid Field.");
+                break;
         }
 
-        public async Async.Task<JobResult?> GetJobResult(Guid jobId) {
-            return await SearchByPartitionKeys(new[] { jobId.ToString() }).SingleOrDefaultAsync();
+        return newResult;
+    }
+
+    public async Async.Task<OneFuzzResult<JobResult>> CreateOrUpdate(Guid jobId, HeartbeatType resultType) {
+
+        var job = await _context.JobOperations.Get(jobId);
+        if (job == null) {
+            return OneFuzzResult<JobResult>.Error(ErrorCode.INVALID_REQUEST, "invalid job");
         }
 
-        public JobResult UpdateResult(JobResult result, HeartbeatType type) {
-            // return await SearchByPartitionKeys(new[] { jobId.ToString() }).SingleOrDefaultAsync();
+        var jobResult = await GetJobResult(jobId);
 
-            var newResult = result;
-            int newValue;
-            switch (type) {
-                case HeartbeatType.NewCrashingInput:
-                    newValue = result.NewCrashingInput + 1;
-                    newResult = result with { NewCrashingInput = newValue };
-                    break;
-                case HeartbeatType.NewReport:
-                    newValue = result.NewReport + 1;
-                    newResult = result with { NewReport = newValue };
-                    break;
-                case HeartbeatType.NewUniqueReport:
-                    newValue = result.NewReport + 1;
-                    newResult = result with { NewReport = newValue };
-                    break;
-                case HeartbeatType.NewRegressionReport:
-                    newValue = result.NewRegressionReport + 1;
-                    newResult = result with { NewRegressionReport = newValue };
-                    break;
-                default:
-                    _logTracer.LogInformation("Invalid Field.");
-                    break;
+        if (jobResult == null) {
+            _logTracer.LogInformation("Creating new JobResult for Job {JobId}", jobId);
+
+            var entry = new JobResult(JobId: jobId, Project: job.Config.Project, Name: job.Config.Name, 0, 0, 0, 0, 0);
+
+            jobResult = UpdateResult(entry, resultType);
+
+            var r = await this.Insert(jobResult);
+            if (!r.IsOk) {
+                _logTracer.AddHttpStatus(r.ErrorV);
+                _logTracer.LogError("failed to insert job result {JobId}", jobResult.JobId);
             }
+            _logTracer.LogInformation("created job result {JobId}", jobResult.JobId);
+        } else {
+            _logTracer.LogInformation("Updating existing JobResult entry for Job {JobId}", jobId);
 
-            return newResult;
+            jobResult = UpdateResult(jobResult, resultType);
+
+            var r = await Replace(jobResult);
+            if (!r.IsOk) {
+                _logTracer.AddHttpStatus(r.ErrorV);
+                _logTracer.LogError("failed to insert job result {JobId}", jobResult.JobId);
+            }
+            _logTracer.LogInformation("updated job result {JobId}", jobResult.JobId);
         }
 
-        public async Async.Task<OneFuzzResult<JobResult>> CreateOrUpdate(Guid jobId, HeartbeatType resultType) {
-
-            var job = await _context.JobOperations.Get(jobId);
-            if (job == null) {
-                return OneFuzzResult<JobResult>.Error(ErrorCode.INVALID_REQUEST, "invalid job");
-            }
-
-            var jobResult = await GetJobResult(jobId);
-
-            if (jobResult == null) {
-                _logTracer.LogInformation("Creating new JobResult for Job {JobId}", jobId);
-
-                var entry = new JobResult(JobId: jobId, Project: job.Config.Project, Name: job.Config.Name, 0, 0, 0, 0, 0);
-
-                jobResult = UpdateResult(entry, resultType);
-
-                var r = await this.Insert(jobResult);
-                if (!r.IsOk) {
-                    _logTracer.AddHttpStatus(r.ErrorV);
-                    _logTracer.LogError("failed to insert job result {JobId}", jobResult.JobId);
-                }
-                _logTracer.LogInformation("created job result {JobId}", jobResult.JobId);
-            } else {
-                _logTracer.LogInformation("Updating existing JobResult entry for Job {JobId}", jobId);
-
-                jobResult = UpdateResult(jobResult, resultType);
-
-                var r = await Replace(jobResult);
-                if (!r.IsOk) {
-                    _logTracer.AddHttpStatus(r.ErrorV);
-                    _logTracer.LogError("failed to insert job result {JobId}", jobResult.JobId);
-                }
-                _logTracer.LogInformation("updated job result {JobId}", jobResult.JobId);
-            }
-
-            return OneFuzzResult<JobResult>.Ok(jobResult);
-        }
+        return OneFuzzResult<JobResult>.Ok(jobResult);
     }
 }
+
