dd8e9d6aace7abf205d3ca74038c71efca1b4e06
Kanan Boubion <kananboubion@microsoft.com>
2023-07-25 11:25:24 -0700

    Change crash_info default values for mypy

--- src/cli/onefuzz/api.py
+++ src/cli/onefuzz/api.py
@@ -558,9 +558,9 @@ class Repro(Endpoint):
         report = json.loads(report_bytes)
 
         crash_info = {
-            "input_blob_container": None,
-            "input_blob_name": None,
-            "job_id": None,
+            "input_blob_container": "",
+            "input_blob_name": "",
+            "job_id": "",
         }
         if "input_blob" in report:
             crash_info["input_blob_container"] = report["input_blob"]["container"]
