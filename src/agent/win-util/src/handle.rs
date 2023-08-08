58447f72f688b048b658bc5600d329e4085b0334
George Pollard <gpollard@microsoft.com>
2023-08-09 09:13:42 +1200

    Migrate from winapi to windows-rs (#3050)
    `windows-rs` is the newer, Microsoft-supported version of the API bindings; `winapi` hasn't been updated in some time. This allows us to remove some code, as `windows-rs` includes the `Sym*` functions that we had to previously defined ourselves in `dbghelp`.
    
    Rather than port the `jobs` and `com` code I removed it, as it was unused.
    
    The `check_hr` and `check_winapi` macros have been replaced by use of `.ok()?` on `BOOL` and `?` on `HRESULT` which now support standard error handling facilities.

--- src/agent/win-util/src/handle.rs
+++ src/agent/win-util/src/handle.rs
@@ -1,16 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use winapi::{
-    shared::minwindef::{FALSE, LPHANDLE},
-    um::{
-        handleapi::{CloseHandle, DuplicateHandle, INVALID_HANDLE_VALUE},
-        processthreadsapi::GetCurrentProcess,
-        winnt::{DUPLICATE_SAME_ACCESS, HANDLE},
+use windows::Win32::{
+    Foundation::{
+        CloseHandle, DuplicateHandle, DUPLICATE_SAME_ACCESS, FALSE, HANDLE, INVALID_HANDLE_VALUE,
     },
+    System::Threading::GetCurrentProcess,
 };
 
 pub struct Handle(pub HANDLE);
+
+impl Handle {
+    fn try_drop(&mut self) -> anyhow::Result<()> {
+        unsafe { CloseHandle(self.0) }.ok()?;
+        self.0 = INVALID_HANDLE_VALUE;
+        Ok(())
+    }
+}
 
 impl Clone for Handle {
     fn clone(&self) -> Self {
@@ -21,7 +27,7 @@ impl Clone for Handle {
                 current_process,
                 self.0,
                 current_process,
-                &mut duplicate as LPHANDLE,
+                &mut duplicate,
                 0,
                 FALSE,
                 DUPLICATE_SAME_ACCESS,
@@ -34,10 +40,33 @@ impl Clone for Handle {
 
 impl Drop for Handle {
     fn drop(&mut self) {
-        unsafe { CloseHandle(self.0) };
+        // ignore any error
+        _ = self.try_drop();
     }
 }
 
 unsafe impl Send for Handle {}
 
 unsafe impl Sync for Handle {}
+
+#[cfg(test)]
+mod test {
+    use windows::Win32::System::Threading::{GetCurrentProcessId, OpenProcess, PROCESS_ALL_ACCESS};
+
+    use super::*;
+
+    #[test]
+    fn handle_clone() {
+        // get a real (not pseudo) handle to play with
+        let mut handle1 = Handle(
+            unsafe { OpenProcess(PROCESS_ALL_ACCESS, false, GetCurrentProcessId()) }.unwrap(),
+        );
+
+        let mut handle2 = handle1.clone();
+        assert_ne!(handle1.0, handle2.0);
+        handle1.try_drop().unwrap();
+        assert_eq!(handle1.0, INVALID_HANDLE_VALUE);
+        handle2.try_drop().unwrap();
+        assert_eq!(handle2.0, INVALID_HANDLE_VALUE);
+    }
+}
