d62cb349d4d63d2236e16a07e51f49921f9aeedd
Noah Harper <noharper@microsoft.com>
2023-09-06 15:36:52 -0700

    Adding update to test branch.

--- src/ci/set-versions.sh
+++ src/ci/set-versions.sh
@@ -6,12 +6,10 @@
 set -ex
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
-GET_VERSION=${SCRIPT_DIR}/get-version.sh
-VERSION=${1:-$(${GET_VERSION})}
 cd ${SCRIPT_DIR}/../../
 
 SET_VERSIONS="src/pytypes/onefuzztypes/__version__.py src/cli/onefuzz/__version__.py"
 SET_REQS="src/cli/requirements.txt"
 
-sed -i "s/0.0.0/${VERSION}/" ${SET_VERSIONS}
-sed -i "s/onefuzztypes~=0.0.0/onefuzztypes==${VERSION}/" ${SET_REQS}
+sed -i 's/__version__ = .*/__version__ = "0.0.0"/' ${SET_VERSIONS}
+sed -i "s/onefuzztypes==.*/onefuzztypes==0.0.0/" ${SET_REQS}
