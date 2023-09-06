72852dcfb1c9c8b0feeae967007be4d35c4aa23d
Noah McGregor Harper <74685766+nharper285@users.noreply.github.com>
2023-09-06 22:06:05 +0000

    trying to replace major minor

--- src/ci/set-versions.sh
+++ src/ci/set-versions.sh
@@ -11,10 +11,11 @@ VERSION=${1:-$(${GET_VERSION})}
 cd ${SCRIPT_DIR}/../../
 
 arrVer=(${VERSION//./ })
-echo "${arrVer[0]}.${arrVer[1]}"  
+MAJOR=${arrVer[0]}
+MINOR=${arrVer[1]}
 
 SET_VERSIONS="src/pytypes/onefuzztypes/__version__.py src/cli/onefuzz/__version__.py"
 SET_REQS="src/cli/requirements.txt"
 
 sed -i "s/0.0.0/${VERSION}/" ${SET_VERSIONS}
-sed -i "s/onefuzztypes>=0.0.0/onefuzztypes>=${VERSION}/" ${SET_REQS}
+sed -i "s/onefuzztypes==0.0.0/onefuzztypes==${MAJOR}.${MINOR}.*/" ${SET_REQS}
