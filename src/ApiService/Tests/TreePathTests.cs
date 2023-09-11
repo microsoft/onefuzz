c7dc91a7e68b154ad19bc079ada4b36d910781ec
Kanan Boubion <kananboubion@microsoft.com>
2023-09-11 13:27:59 -0700

    Format tree path unit tests

--- src/ApiService/Tests/TreePathTests.cs
+++ src/ApiService/Tests/TreePathTests.cs
@@ -1,8 +1,8 @@
-using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.OneFuzz.Service;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
-using System.Collections.Generic;
-using System.Linq;
+using Xunit;
 
 namespace Tests;
 
