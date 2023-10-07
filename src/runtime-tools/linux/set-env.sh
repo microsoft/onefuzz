726b180754265161da1476b78a0d0578fdc1121f
Ragavan Dasarathan <bdasarathan@nvidia.com>
2023-10-07 04:40:13 -0700

    rust trace
    Signed-off-by: Ragavan Dasarathan <bdasarathan@nvidia.com>

--- src/runtime-tools/linux/set-env.sh
+++ src/runtime-tools/linux/set-env.sh
@@ -3,4 +3,4 @@
 export DOTNET_ROOT=/onefuzz/tools/dotnet
 export DOTNET_CLI_HOME="$DOTNET_ROOT"
 export LLVM_SYMBOLIZER_PATH=/onefuzz/bin/llvm-symbolizer
-export RUST_LOG = "info"
+export RUST_LOG = "trace"
