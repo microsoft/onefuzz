64aaefe60c1b9efc80c3aa1ea16580467741f461
Noah McGregor Harper <74685766+nharper285@users.noreply.github.com>
2023-10-11 17:40:42 +0000

    Removing old code.

--- src/ApiService/ApiService/onefuzzlib/JobResultOperations.cs
+++ src/ApiService/ApiService/onefuzzlib/JobResultOperations.cs
@@ -21,12 +21,6 @@ public class JobResultOperations : Orm<JobResult>, IJobResultOperations {
         return await data.FirstOrDefaultAsync();
     }
 
-    // public async Async.Task<ResultVoid<(HttpStatusCode Status, string Reason)>> ToUpdate(string resultType, JobResult oldEntry, JobResult newEntry) => resultType switch {
-    //     "CoverageData" when oldEntry.MetricValue["rate"] < newEntry.MetricValue["rate"] => await Replace(newEntry),
-    //     "RuntimeStats" when oldEntry.MetricValue["total_count"] < newEntry.MetricValue["total_count"] => await Replace(newEntry),
-    //     _ => await Update(oldEntry with { MetricValue = new Dictionary<string, double>() { { "count", oldEntry.MetricValue["count"]++ } } }),
-    // };
-
     private async Async.Task<bool> TryUpdate(Job job, Guid taskId, Guid machineId, string resultType, Dictionary<string, double> resultValue) {
         var jobId = job.JobId;
         var machineIdMetric = string.Concat(machineId, resultType);
@@ -66,38 +60,7 @@ public class JobResultOperations : Orm<JobResult>, IJobResultOperations {
 
         return true;
 
-        // _logTracer.LogInformation($"attempt to replace job result {taskId} and machineId+metricType {machineIdMetric}");
-        // var r = await ToUpdate(resultType, oldEntry, newEntry);
-        // if (!r.IsOk) {
-        //     throw new InvalidOperationException($"failed to replace job result with taskId {taskId} and machineId+metricType {machineIdMetric}");
-        // }
-        // return true;
     }
-    // } else if (resultType.Equals("CoverageData") && jobResult.MetricValue["rate"] < resultValue["rate"]) {
-    //     _logTracer.LogInformation($"attempt to replace coverage job result for {taskId} and machineId+metricType {machineIdMetric}");
-    //     var entry = new JobResult(TaskId: taskId, MachineIdMetric: machineIdMetric, JobId: jobId, Project: job.Config.Project, Name: job.Config.Name, resultType, resultValue);
-    //     var r = await Replace(entry);
-    //     if (!r.IsOk) {
-    //         throw new InvalidOperationException($"failed to replace job result with taskId {taskId} and machineId+metricType {machineIdMetric}");
-    //     }
-    // } else if (resultType.Equals("RuntimeStats") && jobResult.MetricValue["total_count"] < resultValue["total_count"]) {
-    //     _logTracer.LogInformation($"attempt to replace runtime stats job result for {taskId} and machineId+metricType {machineIdMetric}");
-    //     var entry = new JobResult(TaskId: taskId, MachineIdMetric: machineIdMetric, JobId: jobId, Project: job.Config.Project, Name: job.Config.Name, resultType, resultValue);
-    //     var r = await Replace(entry);
-    //     if (!r.IsOk) {
-    //         throw new InvalidOperationException($"failed to replace job result with taskId {taskId} and machineId+metricType {machineIdMetric}");
-    //     }
-    // } else {
-    //     jobResult.MetricValue["count"]++;
-    //     newResultValue = jobResult.MetricValue;
-    //     var newResult = jobResult with { MetricValue = newResultValue };
-    //     _logTracer.LogInformation($"attempt to update job result {taskId} and machineId+metricType {machineIdMetric} with new count: {newResultValue}");
-    //     var r = await Update(newResult);
-    //     if (!r.IsOk) {
-    //         throw new InvalidOperationException($"failed to update job result with taskId {taskId} and machineId+metricType {machineIdMetric}");
-    //     }
-    // }
-    // }
 
     public async Async.Task<OneFuzzResultVoid> CreateOrUpdate(Guid jobId, Guid taskId, Guid machineId, string resultType, Dictionary<string, double> resultValue) {
 
