35f2c2d0c3ec80abb424e0eda6ac644688735c55
Noah McGregor Harper <74685766+nharper285@users.noreply.github.com>
2023-10-17 00:17:21 +0000

    lint

--- src/deployment/deploy.py
+++ src/deployment/deploy.py
@@ -40,8 +40,6 @@ from azure.storage.blob import (
     ContainerSasPermissions,
     generate_container_sas,
 )
-from msrest.serialization import TZ_UTC
-
 from deploylib.configuration import (
     Config,
     InstanceConfigClient,
@@ -67,6 +65,7 @@ from deploylib.registration import (
     set_app_audience,
     update_pool_registration,
 )
+from msrest.serialization import TZ_UTC
 
 # Found by manually assigning the User.Read permission to application
 # registration in the admin portal. The values are in the manifest under
