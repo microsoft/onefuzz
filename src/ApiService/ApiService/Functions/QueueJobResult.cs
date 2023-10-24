42d30f77efa334555e370d5024433b4cc5e7bcd7
Noah McGregor Harper <74685766+nharper285@users.noreply.github.com>
2023-10-24 21:57:09 +0000

    Trying again.

--- src/ApiService/ApiService/Functions/QueueJobResult.cs
+++ src/ApiService/ApiService/Functions/QueueJobResult.cs
@@ -57,7 +57,7 @@ public class QueueJobResult {
             return;
         }
 
-        var jobResult = await _context.JobResultOperations.CreateOrUpdate(job.JobId, jr.TaskId, jr.MachineId, jr.CreatedAt, jobResultType, value);
+        var jobResult = await _context.JobResultOperations.CreateOrUpdate(job.JobId, jr.TaskId, jr.MachineId, jr.CreatedAt.Value, jobResultType, value);
         if (!jobResult.IsOk) {
             _log.LogError("failed to create or update with job result {JobId}", job.JobId);
         }
